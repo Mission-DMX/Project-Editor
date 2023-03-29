13f9209dd1c0e9f19df65e98d49cc028a0d98e87
Niklas Naumann <git@niklasnaumann.de>
2023-03-29 16:05:14 +0200

    Updated setup.py and added __init__.py for widgets.NodeEditor

--- setup.py
+++ setup.py
@@ -1,3 +1,11 @@
 from setuptools import setup, find_packages
 
-setup(name='modulename', packages=find_packages(), package_dir={'': './'}, scripts=['src/DMXGui.py'])
+setup(name='fish-e',
+        version=1.0,
+        description='Editor for realtime dmx control software FISH https://github.com/Mission-DMX/realtime-fish',
+        author='Niklas Naumann',
+        author_email='niklas.naumann@student.uni-luebeck.de',
+        url='https://github.com/Mission-DMX/Project-Editor',
+        packages=find_packages(),
+        package_dir={'': './'},
+        scripts=['src/DMXGui.py'])
