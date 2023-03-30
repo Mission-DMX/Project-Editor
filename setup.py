0a6d42e7a24a826135436db8d327f517ecc15bbc
MKay <ma.kaussow@uni-luebeck.de>
2023-03-30 13:22:54 +0200

    beautification

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(name='fish-e',
-        version=1.0,
-        description='Editor for realtime dmx control software FISH https://github.com/Mission-DMX/realtime-fish',
-        author='Niklas Naumann',
-        author_email='niklas.naumann@student.uni-luebeck.de',
-        url='https://github.com/Mission-DMX/Project-Editor',
-        requires=['PySide6', 'protobuf', 'qtpynodeeditor'],
-        packages=find_packages(),
-        package_dir={'': './'},
-        scripts=['src/DMXGui.py'])
+      version='1.0',
+      description='Editor for realtime dmx control software FISH https://github.com/Mission-DMX/realtime-fish',
+      author='Niklas Naumann',
+      author_email='niklas.naumann@student.uni-luebeck.de',
+      url='https://github.com/Mission-DMX/Project-Editor',
+      requires=['PySide6', 'protobuf', 'qtpynodeeditor'],
+      packages=find_packages(),
+      package_dir={'': './'},
+      scripts=['src/DMXGui.py'])
