dfa14d27e83508a6fe8bd4c9a947bd1f6465f516
MKay <ma.kaussow@uni-luebeck.de>
2023-03-30 14:15:02 +0200

    network to proto

--- src/Network.py
+++ src/Network.py
@@ -22,7 +22,7 @@ class NetworkManager(QtCore.QObject):
 #        self._socket.setServerName("/var/run/fish.sock")
         self._socket.setServerName("/tmp/fish.sock")
         self._socket.stateChanged.connect(self._on_state_changed)
-        self._socket.errorOccurred.connect(self.on_error)
+        self._socket.errorOccurred.connect(on_error)
         self._socket.readyRead.connect(self.on_ready_read)
 
     def start(self):
@@ -38,23 +38,39 @@ class NetworkManager(QtCore.QObject):
         """
         print(f"connection change to {str(self._socket.state())}")
 
-    def on_error(self, error):
-        print(error)
-
     def send(self, universe: Universe) -> None:
         """
         Sends the current dmx data of an universes.#
 
-        :param universe: universe to send to fish
-        """
-        if self._socket.state() == QtNetwork.QAbstractSocket.SocketState.ConnectedState:
-            msg = proto.DirectMode_pb2.dmx_output(universe_id=universe.address,
-                                                  channel_data=[channel.value for channel in universe.channels])
-            self._socket.write(bytearray(str(msg), encoding='utf8'))
+#        :param universe: universe to send to fish
+#        """
+        #       if self._socket.state() == QtNetwork.QAbstractSocket.SocketState.ConnectedState:
 
-    def _read(self) -> QtCore.QByteArray:
-        return self._socket.readAll()
+        msg = proto.DirectMode_pb2.dmx_output(universe_id=universe.address,
+                                              channel_data=[channel.value for channel in universe.channels])
+
+        self.send_with_format(msg.SerializeToString(), proto.MessageTypes_pb2.MSGT_DMX_OUTPUT)
+
+    def generate_universe(self, universe: Universe) -> None:
+        print(self._socket.state())
+        # if self._socket.state() == QtNetwork.QAbstractSocket.SocketState.ConnectedState:
+
+        msg = proto.UniverseControl_pb2.Universe(id=1,
+                                                 remote_location=proto.UniverseControl_pb2.Universe.ArtNet(
+                                                     ip_address="192.168.0.2",
+                                                     port=6454,
+                                                     universe_on_device=1
+                                                 ))
+        self.send_with_format(msg.SerializeToString(), proto.MessageTypes_pb2.MSGT_UNIVERSE)
+
+    def send_with_format(self, msg: bytearray, msg_type: proto.MessageTypes_pb2):
+        self._socket.write(varint.encode(msg_type) + varint.encode(len(msg)) + msg)
 
     def on_ready_read(self):
         """Processes incoming data."""
-        print(f"Response: {self._socket.readAll()}")
+
+
+#        print(f"Response: {self._socket.readAll()}")
+
+def on_error(error):
+    print(error)
