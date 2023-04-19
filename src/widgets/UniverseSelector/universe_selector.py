2e312dc7bdf2a4dded50a2a496dca84a2d7c7499
MKay <ma.kaussow@uni-luebeck.de>
2023-04-19 10:50:56 +0200

    universe_selector update vor szeneEditor

--- src/widgets/UniverseSelector/universe_selector.py
+++ src/widgets/UniverseSelector/universe_selector.py
@@ -13,21 +13,35 @@ from widgets.PatchPlan.patch_plan_widget import PatchPlanWidget
 class UniverseSelector(QtWidgets.QTabWidget):
     """select Universe from Tab Widget"""
 
-    def __init__(self, universes: list[Universe], fish_connector: NetworkManager, parent=None) -> None:
+    def __init__(self, fish_connector: NetworkManager, universes: list[Universe] = None, parent=None) -> None:
         super().__init__(parent=parent)
+        if universes is None:
+            self._universes: list[Universe] = [Universe(1)]
+        else:
+            self._universes: list[Universe] = universes
         self._fish_connector = fish_connector
         self.setStyleSheet(Style.WIDGET)
         self.setTabPosition(QtWidgets.QTabWidget.TabPosition.North)
 
-        for universe in universes:
+        for universe in self._universes:
             self.add_universe(universe)
 
-    def add_universe(self, universe: Universe) -> None:
+    @property
+    def universes(self) -> list[Universe]:
+        """Universes property"""
+        return self._universes
+
+    def add_universe(self, universe: Universe = None) -> None:
         """
         add a new Universe to universe Selector
         Args:
             universe: the new universe to select
         """
+        if universe is None:
+            universe = Universe(len(self._universes) + 1)
+        if universe not in self._universes:
+            self._universes.append(universe)
+
         if self._fish_connector.is_running:
             self._fish_connector.generate_universe(universe)
             self._fish_connector.send_universe(universe)
@@ -42,3 +56,10 @@ class UniverseSelector(QtWidgets.QTabWidget):
         splitter.addWidget(direct_editor)
 
         self.addTab(splitter, str(universe.address))
+
+    def universe_coppy(self) -> list[Universe]:
+        universes_copy: list[Universe] = []
+        for universe in self._universes:
+            universe_copy = Universe(universe.address)
+            universes_copy.append(universe_copy)
+        return universes_copy
