59b9d929c9f978e125ba4564b501efd8893c650e
MKay <ma.kaussow@uni-luebeck.de>
2023-05-15 18:21:31 +0200

    scene editor add tab

--- src/view/direct_mode/SceneEditor/scene_editor.py
+++ src/view/direct_mode/SceneEditor/scene_editor.py
@@ -5,7 +5,7 @@ from PySide6 import QtWidgets, QtGui
 from Network import NetworkManager
 from Style import Style
 from ofl.fixture import UsedFixture
-from widgets.UniverseSelector.universe_selector import UniverseSelector
+from view.direct_mode.SceneEditor.UniverseSelector.universe_selector import UniverseSelector
 
 
 class SceneEditor(QtWidgets.QTabWidget):
@@ -16,9 +16,10 @@ class SceneEditor(QtWidgets.QTabWidget):
         self._fish_connector = fish_connector
         self.setStyleSheet(Style.WIDGET)
         self.setTabPosition(QtWidgets.QTabWidget.TabPosition.West)
-        self._scenes: list[UniverseSelector] = [UniverseSelector(self._fish_connector, None, self)]
-        self.addTab(self._scenes[0], "start")
-        self.currentChanged.connect(self.tab_changed)
+        self._scenes: list[UniverseSelector] = []
+        self.addTab(QtWidgets.QWidget(), "+")
+        self.currentChanged.connect(self._tab_changed)
+        self.tabBarClicked.connect(self._tab_clicked)
 
     @property
     def scenes(self) -> list[UniverseSelector]:
@@ -27,31 +28,31 @@ class SceneEditor(QtWidgets.QTabWidget):
 
     def contextMenuEvent(self, event):
         """context menu"""
-        for index in range(self.tabBar().count()):
+        for index in range(self.tabBar().count() - 1):
             if self.tabBar().tabRect(index).contains(event.pos()):
                 menu = QtWidgets.QMenu(self)
-                add_scene = QtGui.QAction('add Scene', self)
+
                 rename_scene = QtGui.QAction('rename Scene', self)
                 delete_scene = QtGui.QAction('delete Scene', self)
-                add_scene.triggered.connect(lambda: self.add_scene())
-                rename_scene.triggered.connect(
-                    lambda: self._rename_scene(index))
+                rename_scene.triggered.connect(lambda: self._rename_scene(index))
                 delete_scene.triggered.connect(lambda: self._remove_scene(index))
-                menu.addAction(add_scene)
                 menu.addAction(rename_scene)
                 menu.addAction(delete_scene)
                 menu.popup(QtGui.QCursor.pos())
                 break
 
-    def add_scene(self) -> None:
+    def _add_scene(self) -> None:
         """
         add a new scene
         """
         text, ok = QtWidgets.QInputDialog.getText(self, "Scene Name", "Enter Scene Name:")
         if ok:
-            self._scenes.append(
-                UniverseSelector(self._fish_connector, self._scenes[0].universe_coppy(), self))
-            self.addTab(self._scenes[-1], text)
+            if self._scenes:
+                self._scenes.append(
+                    UniverseSelector(self._fish_connector, self._scenes[0].universe_coppy(), self))
+            else:
+                self._scenes.append(UniverseSelector(self._fish_connector, None, self))
+            self.insertTab(self.tabBar().count() - 1, self._scenes[-1], text)
 
     def _rename_scene(self, index: int) -> None:
         text, ok = QtWidgets.QInputDialog.getText(self, f"Rename {self.tabText(index)} ", "Enter new Scene Name:")
@@ -61,6 +62,8 @@ class SceneEditor(QtWidgets.QTabWidget):
     def _remove_scene(self, index: int) -> None:
         self.removeTab(index)
         self._scenes.remove(self._scenes[index])
+        if index == len(self._scenes) and index != 0:
+            self.tabBar().setCurrentIndex(index - 1)
 
     def add_universe(self) -> None:
         """ add a new universe """
@@ -72,13 +75,22 @@ class SceneEditor(QtWidgets.QTabWidget):
         for scene in self._scenes:
             scene.start()
 
-    def tab_changed(self, scene_index: int) -> None:
+    def _tab_changed(self, scene_index: int) -> None:
         """
         send all universes if tab changed
         Args:
             scene_index: index of current scene
         """
-        self._scenes[scene_index].send_all_universe()
+        print(scene_index)
+        if scene_index == self.tabBar().count() - 1:
+            self.tabBar().setCurrentIndex(self._last_tab)
+        else:
+            self._scenes[scene_index].send_all_universe()
+
+    def _tab_clicked(self, scene_index: int) -> None:
+        self._last_tab = self.tabBar().currentIndex()
+        if scene_index == self.tabBar().count() - 1:
+            self._add_scene()
 
     def patch(self, fixture: UsedFixture, patching: str) -> None:
         """
