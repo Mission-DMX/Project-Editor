19a3499cccc4eb30f7f2db82cc5fb3606384f4a4
Doralitze <doralitze@chaotikum.org>
2023-05-17 10:37:56 +0200

    doc: remote_control_port.py

--- src/cli/remote_control_port.py
+++ src/cli/remote_control_port.py
@@ -7,7 +7,13 @@ from cli.cli_context import CLIContext
 
 
 class SocketStreamReader:
+    """This class is used to split the input TCP stream into separate lines."""
     def __init__(self, sock: socket):
+        """Initialize the stream reader
+        
+        Arguments:
+        sock -- the socket to listen on
+        """
         self._sock = sock
         self._recv_buffer = bytearray()
         self.echo = True
@@ -16,6 +22,7 @@ class SocketStreamReader:
         raise NotImplementedError
 
     def read_exactly(self, num_bytes: int) -> bytes:
+        """Read num_bytes from the socket."""
         buf = bytearray(num_bytes)
         pos = 0
         while pos < num_bytes:
@@ -26,9 +33,22 @@ class SocketStreamReader:
         return bytes(buf)
 
     def readline(self) -> bytes:
+        """Read an entire line from the socket stream.
+        
+        Returns:
+        bytearray with line.
+        """
         return self.read_until(b"\n")
 
     def read_until(self, separator: bytes = b"\n") -> bytes:
+        """Read from the socket until the escape sequence was found
+        
+        Arguments:
+        separator -- the delimitter to look out for
+        
+        Returns:
+        bytearray with the found content.
+        """
         if len(separator) != 1:
             raise ValueError("Only separators of length 1 are supported.")
 
@@ -54,6 +74,7 @@ class SocketStreamReader:
         return result
 
     def _recv_into(self, view: memoryview) -> int:
+        """This method performs a zero copy read request."""
         bytes_read = min(len(view), len(self._recv_buffer))
         view[:bytes_read] = self._recv_buffer[:bytes_read]
         self._recv_buffer = self._recv_buffer[bytes_read:]
@@ -67,7 +88,15 @@ class SocketStreamReader:
 
 
 class Connection:
+    """This class handles a remote CLI connection."""
     def __init__(self, client: socket, address: str, connection_map: dict):
+        """This constructor takes over the connection.
+        
+        Arguments:
+        client -- the connection socket fd.
+        address -- the remote address of the connected client.
+        connection_map -- the map handling all active connections.
+        """
         self.context = CLIContext(exit_available=True)
         self._client = client
         self._remote_address = address
@@ -76,6 +105,7 @@ class Connection:
         self._client_thread.start()
 
     def run(self):
+        """This method will be called by the client thread in order to handle the client."""
         try:
             reader = SocketStreamReader(self._client)
             while not self.context.exit_called:
@@ -93,13 +123,25 @@ class Connection:
         self._connection_map.pop(self._remote_address)
 
     def stop(self):
+        """Forcefully disconnect and stop the client.
+        
+        This operation may block until the operating system released the socket.
+        """
         self.context.exit_called = True
         self._client.close()
         self._client_thread.join()
 
 
 class RemoteCLIServer:
+    """This class handles the control port. Only IPv6 connections are supported."""
+    
     def __init__(self, interface: str = "::", port: int = 2929):
+        """Construct the handler and opens a port.
+        
+        Arguments:
+        interface -- The interface to bind to. Defaults to all IPv6 interfaces.
+        port -- The port to listen on. Defaults to TCP/2929
+        """
         self._server_thread = Thread(target=self.run)
         self._bind_interface = interface
         self._bind_port = port
@@ -109,6 +151,7 @@ class RemoteCLIServer:
         self._server_thread.start()
 
     def run(self):
+        """This method will be run by the server thread in order to process incomming clients."""
         with socket(AF_INET6, SOCK_STREAM) as s:
             self._server_socket = s
             s.bind((self._bind_interface, self._bind_port))
@@ -123,6 +166,10 @@ class RemoteCLIServer:
                     pass
 
     def stop(self):
+        """This method stops the server and disconnects all clients.
+        
+        It may block until the operating system released all resources.
+        """
         self._stopped = True
         self._server_socket.close()
         to_be_stopped = []
