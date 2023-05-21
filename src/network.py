1aa0ac485e33f54bdce1af2c0b49d091cd0d7a38
MKay <ma.kaussow@uni-luebeck.de>
2023-05-21 19:30:28 +0200

    type error

--- src/network.py
+++ src/network.py
@@ -49,7 +49,7 @@ class NetworkManager(QtCore.QObject):
 
         self._broadcaster.view_is_patch_menu.connect(self._send_view_patch_menu)
         self._broadcaster.view_is_patching.connect(self._send_view_patching)
-        self._broadcaster.view_is_not_patch_menu(self._send_view_not_patch_menu)
+        self._broadcaster.view_is_not_patch_menu.connect(self._send_view_not_patch_menu)
         self._message_queue = queue.Queue()
 
     @property
