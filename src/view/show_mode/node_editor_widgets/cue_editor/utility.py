518ebd80e52e40b29ed99e037cb484fb8d204455
Leon Dietrich <doralitze@chaotikum.org>
2023-05-25 13:28:57 +0200

    fix: time display

--- src/view/show_mode/node_editor_widgets/cue_editor/utility.py
+++ src/view/show_mode/node_editor_widgets/cue_editor/utility.py
@@ -2,4 +2,5 @@ import time
 
 
 def format_seconds(seconds: float):
-    return time.strftime('%H:%M:%S.{}'.format(int((seconds * 1000) % 1000), time.gmtime(int(seconds))))
+    millis = int((seconds * 1000) % 1000)
+    return time.strftime('%H:%M:%S.{}'.format(millis), time.gmtime(int(seconds)))
