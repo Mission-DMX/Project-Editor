d10695dae58d5c791ba05b8c751f52573dfb2589
MKay <ma.kaussow@uni-luebeck.de>
2023-05-28 22:56:49 +0200

    xtouch messages for color and Temperature

--- src/x_touch.py
+++ src/x_touch.py
@@ -21,6 +21,20 @@ VIEW_NOT_FILTER_MENU_MSG: proto.Console_pb2.button_state_change = proto.Console_
     button=proto.Console_pb2.ButtonCode.BTN_TRACK_EDITSHOW,
     new_state=proto.Console_pb2.ButtonState.BS_SET_LED_NOT_ACTIVE)
 
+VIEW_COLOR_MSG: proto.Console_pb2.button_state_change = proto.Console_pb2.button_state_change(
+    button=proto.Console_pb2.ButtonCode.BTN_DROP_COLOR, new_state=proto.Console_pb2.ButtonState.BS_ACTIVE)
+
+VIEW_NOT_COLOR_MSG: proto.Console_pb2.button_state_change = proto.Console_pb2.button_state_change(
+    button=proto.Console_pb2.ButtonCode.BTN_DROP_COLOR, new_state=proto.Console_pb2.ButtonState.BS_SET_LED_NOT_ACTIVE)
+
+VIEW_TEMPERATURE_MSG: proto.Console_pb2.button_state_change = proto.Console_pb2.button_state_change(
+    button=proto.Console_pb2.ButtonCode.BTN_REPLACE_TEMPERATURE, new_state=proto.Console_pb2.ButtonState.BS_ACTIVE)
+
+VIEW_NOT_TEMPERATURE_MSG: proto.Console_pb2.button_state_change = proto.Console_pb2.button_state_change(
+    button=proto.Console_pb2.ButtonCode.BTN_REPLACE_TEMPERATURE,
+    new_state=proto.Console_pb2.ButtonState.BS_SET_LED_NOT_ACTIVE)
+
+
 class XTouchMessages:
     """messages to the XTouch"""
 
@@ -36,3 +50,9 @@ class XTouchMessages:
 
         self._broadcaster.view_to_file_editor.connect(lambda: send(VIEW_FILTER_MENU_MSG))
         self._broadcaster.view_leave_file_editor.connect(lambda: send(VIEW_NOT_FILTER_MENU_MSG))
+
+        self._broadcaster.view_to_color.connect(lambda: send(VIEW_COLOR_MSG))
+        self._broadcaster.view_leave_color.connect(lambda: send(VIEW_NOT_COLOR_MSG))
+
+        self._broadcaster.view_to_temperature.connect(lambda: send(VIEW_TEMPERATURE_MSG))
+        self._broadcaster.view_leave_temperature.connect(lambda: send(VIEW_NOT_TEMPERATURE_MSG))
