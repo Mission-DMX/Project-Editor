880958d02b69d3d81fd61bc8a12b51e16a0e3204
MKay <ma.kaussow@uni-luebeck.de>
2023-05-28 22:55:10 +0200

    add color and temperature signals

--- src/model/broadcaster.py
+++ src/model/broadcaster.py
@@ -50,6 +50,12 @@ class Broadcaster(QtCore.QObject, metaclass=QObjectSingletonMeta):
 
     view_leave_colum_select: QtCore.Signal = QtCore.Signal()
     view_change_colum_select: QtCore.Signal = QtCore.Signal()
+
+    view_to_color: QtCore.Signal = QtCore.Signal()
+    view_leave_color: QtCore.Signal = QtCore.Signal()
+
+    view_to_temperature: QtCore.Signal = QtCore.Signal()
+    view_leave_temperature: QtCore.Signal = QtCore.Signal()
     ################################################################
     jogwheel_rotated_left: QtCore.Signal = QtCore.Signal()
     jogwheel_rotated_right: QtCore.Signal = QtCore.Signal()
