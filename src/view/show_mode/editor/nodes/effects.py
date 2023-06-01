3c13c471a165355e4214274252533eb26a703964
Ludwig Rahlff <ludwig.rahlff@student.uni-luebeck.de>
2023-06-01 21:23:15 +0200

    fix: cuefilter outputs are generated while loading

--- src/view/show_mode/editor/nodes/effects.py
+++ src/view/show_mode/editor/nodes/effects.py
@@ -11,8 +11,11 @@ class CueListNode(FilterNode):
         super().__init__(model=model, filter_type=44, name=name, terminals={
             'time': {'io': 'in'}},
                          allowAddOutput=True)
+
         try:
-            self.filter.initial_parameters["mapping"] = model.initial_parameters["mapping"]
+            mapping_from_file = model.initial_parameters["mapping"]
+            self.filter.initial_parameters["mapping"] = mapping_from_file
+            self.parse_and_add_output_channels(mapping_from_file)
         except:
             self.filter.initial_parameters["mapping"] = ""
 
@@ -28,3 +31,12 @@ class CueListNode(FilterNode):
 
         self.filter.in_data_types["time"] = DataType.DT_DOUBLE
 
+    def parse_and_add_output_channels(self, mappings: str):
+        for channel_dev in mappings.split(';'):
+            if channel_dev:
+                splitted_channel_dev = channel_dev.split(':')
+                if len(splitted_channel_dev) > 1:
+                    channel_name = splitted_channel_dev[0]
+                    channel_type = DataType.from_filter_str(splitted_channel_dev[1])
+                    self.addOutput(channel_name)
+                    self.filter.out_data_types[channel_name] = channel_type
