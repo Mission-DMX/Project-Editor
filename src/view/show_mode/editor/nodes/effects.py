7d19c35c78ee40642aa2481cc214f39385dc20ce
Leon Dietrich <doralitze@chaotikum.org>
2023-07-26 15:54:18 +0200

    chg: reincluded base terminals

--- src/view/show_mode/editor/nodes/effects.py
+++ src/view/show_mode/editor/nodes/effects.py
@@ -44,11 +44,11 @@ class CueListNode(FilterNode):
 
 class ShiftFilterNode(FilterNode):
     def __init__(self, model, name, id: int, data_type: DataType):
-        super().__init__(model=model, filter_type=id, name=name, allowAddOutput=True) #, terminals={
-            #'input': {'io': 'in'},
-            #'switch_time': {'io', 'in'},
-            #'time': {'io': 'in'},
-        #}, )
+        super().__init__(model=model, filter_type=id, name=name, allowAddOutput=True, terminals={
+            'input': {'io': 'in'},
+            'switch_time': {'io', 'in'},
+            'time': {'io': 'in'},
+        }, )
 
         self.filter.in_data_types["input"] = data_type
         self.filter.in_data_types["switch_time"] = DataType.DT_DOUBLE
