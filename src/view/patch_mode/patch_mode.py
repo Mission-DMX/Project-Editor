ba4f1d16cff32fc4ab0dc9dfd1c30e6b7aabd2e1
MKay <ma.kaussow@uni-luebeck.de>
2023-10-03 18:37:54 +0200

    remove save and load from patch_mode

--- src/view/patch_mode/patch_mode.py
+++ src/view/patch_mode/patch_mode.py
@@ -26,15 +26,6 @@ class PatchMode(QtWidgets.QStackedWidget):
         self._broadcaster.view_patching.connect(lambda: self.setCurrentIndex(1))
         self._broadcaster.view_leave_patching.connect(lambda: self.setCurrentIndex(0))
 
-        self._toolbar: list[QtGui.QAction] = []
-        #        save_button = QtGui.QAction("Save")
-        load_button = QtGui.QAction("Load")
-        #        save_button.triggered.connect(self._save_patching)
-        load_button.triggered.connect(self._load_patching)
-
-        #        self._toolbar.append(save_button)
-        self._toolbar.append(load_button)
-
     @property
     def toolbar(self) -> list[QtGui.QAction]:
         """toolbar for Console mode"""
@@ -49,52 +40,3 @@ class PatchMode(QtWidgets.QStackedWidget):
         if connected:
             for universe in self._broadcaster.patching_universes:
                 self._broadcaster.send_universe.emit(universe)
-
-    def _save_patching(self):
-        msgbox = QMessageBox()
-        msgbox.setText("Diese Funktion sollte nicht mehr verwendet werden. Sie wird bald entfernt.")
-        msgbox.open()
-        data: str = ""
-        for universe in self._broadcaster.patching_universes:
-            for channel in universe.patching:
-                data += ""
-                for channel in universe.channels:
-                    data += str(channel.value) + ","
-                data = data[:-1]
-                data += ";"
-            data = data[:-1]
-            data += "\n"
-        file_name, _ = QtWidgets.QFileDialog.getSaveFileName(self, "save Scene", "",
-                                                             "Text Files (*.txt);;All Files (*)", )
-        if file_name:
-            with open(file_name, "w", encoding='UTF-8') as file:
-                file.write(data)
-
-    #        file_name, _ = QtWidgets.QFileDialog.getSaveFileName(self, "save Scene", "",
-    #                                                             "Json Files (*.json);;All Files (*)", )
-    #        if file_name:
-    #            with open(file_name, "w", encoding='UTF-8') as file:
-    #                file.write(
-    #                    json.dumps(jsonpickle.encode(self._broadcaster.patching_universes, unpicklable=False), indent=4))
-
-    #    def _load_patching(self):
-    #        file_name, _ = QtWidgets.QFileDialog.getOpenFileName(self, "load Scene", "",
-    #                                                             "Json Files (*.json);;All Files (*)", )
-    #        if file_name:
-    #            with open(file_name, "r", encoding='UTF-8') as file:
-    #                data=json.loads(jsonpickle.decode(file.read()))
-    #                for universe in data:
-    #                    print(universe["_universe_proto"])
-    #                    PatchingUniverse(universe["_universe_proto"])
-    ##                    for channel in universe:
-
-    # self._broadcaster.patching_universes=json.loads(jsonpickle.decode(file.read()))
-
-    # print(self._broadcaster.patching_universes[0].universe_proto)
-    # print(json.loads(jsonpickle.decode(file.read())))
-    def _load_patching(self):
-        msgbox = QMessageBox()
-        msgbox.setText("Diese Funktion wurde entfernt. Die Patchdaten werden demnächst in der Showfile mitgespeichert"
-                       "werden")
-        msgbox.open()
-        pass
