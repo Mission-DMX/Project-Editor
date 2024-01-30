3f0b69cbd12626824b8d3a3fa8ec95369f833199
MKay <ma.kaussow@uni-luebeck.de>
2024-01-30 12:40:59 +0100

    logging_widget listen to Broadcast of logg_messag

--- src/view/logging_mode/logging_widget.py
+++ src/view/logging_mode/logging_widget.py
@@ -1,23 +1,11 @@
 # coding=utf-8
 """widget for logging_mode"""
-
+import json
 import logging
 
 from PySide6 import QtWidgets
 
-
-class LoggingHandler(logging.Handler):
-    """logging_mode Handler"""
-
-    def __init__(self, parent) -> None:
-        super().__init__()
-        self.widget = QtWidgets.QPlainTextEdit(parent)
-        self.widget.setReadOnly(True)
-
-    def emit(self, record: logging.LogRecord) -> None:
-        """emit logging message"""
-        msg = self.format(record)
-        self.widget.appendPlainText(msg)
+from model.broadcaster import Broadcaster
 
 
 class LoggingWidget(QtWidgets.QTabWidget):
@@ -26,13 +14,21 @@ class LoggingWidget(QtWidgets.QTabWidget):
     def __init__(self, parent=None) -> None:
         super().__init__(parent)
 
-        log_text_box = LoggingHandler(self)
-        log_text_box.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
-        logging.getLogger().addHandler(log_text_box)
+        self._broadcaster = Broadcaster()
+
+        self._widget = QtWidgets.QPlainTextEdit(parent)
+        self._widget.setReadOnly(True)
+        self._broadcaster.log_message.connect(self.new_log_message)
 
         layout = QtWidgets.QVBoxLayout()
-        # Add the new logging box widget to the layout
-        layout.addWidget(log_text_box.widget)
+        layout.addWidget(self._widget)
         self.setLayout(layout)
 
         logging.info("start DMXGui")
+
+    def new_log_message(self, massage: str):
+        """ handle incoming log messages """
+        print(massage)
+        massage_content: dict = json.loads(massage)
+
+        self._widget.appendPlainText(massage_content["level"])
