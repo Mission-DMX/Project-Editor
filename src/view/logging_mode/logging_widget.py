2b22f949c1d3a8a9244504ee4e1ef8457ef95055
MKay <ma.kaussow@uni-luebeck.de>
2024-02-01 12:31:15 +0100

    add search in logs

--- src/view/logging_mode/logging_widget.py
+++ src/view/logging_mode/logging_widget.py
@@ -2,6 +2,7 @@
 """widget for logging_mode"""
 import json
 import logging
+from typing import List
 
 from PySide6 import QtWidgets
 from PySide6.QtCore import Qt
@@ -18,7 +19,8 @@ class LoggingWidget(QtWidgets.QTabWidget):
         self._broadcaster = Broadcaster()
         self._broadcaster.log_message.connect(self.new_log_message)
 
-        self._searchbar = QtWidgets.QLineEdit()
+        searchbar = QtWidgets.QLineEdit()
+        searchbar.textChanged.connect(self.update_display)
 
         self._scroll = QtWidgets.QScrollArea()
         self._scroll.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
@@ -26,15 +28,27 @@ class LoggingWidget(QtWidgets.QTabWidget):
         self._scroll.setWidgetResizable(True)
         self._scroll.setLayout(QtWidgets.QVBoxLayout())
 
+        self._log_items: List[LoggingItemWidget] = []
+
         container_layout = QtWidgets.QVBoxLayout()
-        container_layout.addWidget(self._searchbar)
+        container_layout.addWidget(searchbar)
         container_layout.addWidget(self._scroll)
 
         self.setLayout(container_layout)
 
         logging.info("start DMXGui")
 
-    def new_log_message(self, message: str):
+    def new_log_message(self, message: str) -> None:
         """ handle incoming log messages """
         massage_content: dict = json.loads(message)
-        self._scroll.layout().addWidget(LoggingItemWidget(self._scroll, massage_content["message"]))
+        new_log_item: LoggingItemWidget = LoggingItemWidget(self._scroll, massage_content["message"])
+        self._log_items.append(new_log_item)
+        self._scroll.layout().addWidget(new_log_item)
+
+    def update_display(self, text: str) -> None:
+        """update display for searching items"""
+        for widget in self._log_items:
+            if text.lower() in widget.content.lower():
+                widget.show()
+            else:
+                widget.hide()
