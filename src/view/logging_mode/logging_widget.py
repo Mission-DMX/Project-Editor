c84d716b5057eaa24650dd38c7c200e0fe4f4554
MKay <ma.kaussow@uni-luebeck.de>
2024-02-01 12:21:31 +0100

    logging widget construct new logging itmes

--- src/view/logging_mode/logging_widget.py
+++ src/view/logging_mode/logging_widget.py
@@ -4,8 +4,10 @@ import json
 import logging
 
 from PySide6 import QtWidgets
+from PySide6.QtCore import Qt
 
 from model.broadcaster import Broadcaster
+from .logging_item_widget import LoggingItemWidget
 
 
 class LoggingWidget(QtWidgets.QTabWidget):
@@ -13,21 +15,26 @@ class LoggingWidget(QtWidgets.QTabWidget):
 
     def __init__(self, parent=None) -> None:
         super().__init__(parent)
-
         self._broadcaster = Broadcaster()
-
-        self._widget = QtWidgets.QPlainTextEdit(parent)
-        self._widget.setReadOnly(True)
         self._broadcaster.log_message.connect(self.new_log_message)
 
-        layout = QtWidgets.QVBoxLayout()
-        layout.addWidget(self._widget)
-        self.setLayout(layout)
+        self._searchbar = QtWidgets.QLineEdit()
+
+        self._scroll = QtWidgets.QScrollArea()
+        self._scroll.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        self._scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self._scroll.setWidgetResizable(True)
+        self._scroll.setLayout(QtWidgets.QVBoxLayout())
+
+        container_layout = QtWidgets.QVBoxLayout()
+        container_layout.addWidget(self._searchbar)
+        container_layout.addWidget(self._scroll)
+
+        self.setLayout(container_layout)
 
         logging.info("start DMXGui")
 
     def new_log_message(self, message: str):
         """ handle incoming log messages """
         massage_content: dict = json.loads(message)
-
-        self._widget.appendPlainText(massage_content["message"])
+        self._scroll.layout().addWidget(LoggingItemWidget(self._scroll, massage_content["message"]))
