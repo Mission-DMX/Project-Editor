e01d101f7c68b4da92ed53ca42a9cc20cfe7f1c5
Leon Dietrich <doralitze@chaotikum.org>
2024-02-03 16:02:53 +0100

    add: documentation for fish_optimizer module

--- src/controller/file/serializing/fish_optimizer.py
+++ src/controller/file/serializing/fish_optimizer.py
@@ -8,13 +8,25 @@ logger = logging.Logger(__file__)
 
 
 class SceneOptimizerModule:
+    """
+    This class contains information required for performing post-processing on a single scene.
+    """
+
     def __init__(self):
         self.channel_override_dict: dict[str, str] = dict()
         self.channel_link_list: list[tuple[Filter, ElementTree.SubElement]] = []
         self._global_time_input_filter: Filter | None = None
         self._main_brightness_input_filter: Filter | None = None
 
-    def filter_was_substituted(self, f: Filter):
+    def filter_was_substituted(self, f: Filter) -> bool:
+        """
+        This method receives a filter and checks if it can be substituted with an equivalent filter that was already
+        placed. If this turns out to be the case, this method fills the output port substitution dictionary with
+        information for the given filter. Otherwise, false will be returned.
+
+        :param f: The filter to check for substitution.
+        :returns: true if the filter was scheduled to be substituted and therefore should not be emplaced for transmission to fish.
+        """
         match f.filter_type:
             case FilterTypeEnumeration.FILTER_TYPE_TIME_INPUT:
                 if self._global_time_input_filter is not None:
@@ -34,6 +46,12 @@ class SceneOptimizerModule:
                 return False
 
     def _fill_ch_sub_dict(self, f: Filter, substitution_filter: Filter):
+        """
+        This method is used to fill the substitution dictionary with required port mappings.
+
+        :param f: The filter that should be substituted
+        :param substitution_filter: The filter that was already emplaced and should be used as a substitution.
+        """
         if f.filter_type != substitution_filter.filter_type:
             raise ValueError("Cannot substitute two filters of different type.")
         if f.is_virtual_filter:
