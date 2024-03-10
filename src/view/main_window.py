8c3d88106a3a3433c11bba2aa892995866024938
MKay <ma.kaussow@uni-luebeck.de>
2024-03-10 13:25:54 +0100

    remove unused

--- src/view/main_window.py
+++ src/view/main_window.py
@@ -41,8 +41,6 @@ class MainWindow(QtWidgets.QMainWindow):
         from model.ui_configuration import setup_network_manager
         setup_network_manager(self._fish_connector)
 
-        #from file.read import read_document #todo remove me xxx
-        #read_document("/home/fish/Desktop/debug/workinprogress.xml",self._board_configuration)
         # views
         views: list[tuple[str, QtWidgets.QWidget, callable]] = [
             ("Console Mode", MainWidget(ConsoleSceneSelector(self), self), lambda: self._to_widget(0)),
