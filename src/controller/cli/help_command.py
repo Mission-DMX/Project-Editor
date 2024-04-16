061bc3091245cbb63d373aa16ebe2fdff7d6633c
Leon Dietrich <doralitze@chaotikum.org>
2024-04-16 13:29:40 +0200

    fix: closing on help request

--- src/controller/cli/help_command.py
+++ src/controller/cli/help_command.py
@@ -38,6 +38,8 @@ class HelpCommand(Command):
             case "bank_set":
                 self.context.print("Modify the selected bank set. Usage: bank_set commit/create <description>"
                                    "/add --bank <bank> --col-type <type>/info/activate")
+            case "showctl":
+                self.context.print("Manage the general show file and execution on fish")
             case _:
                 self.context.print("ERROR: The requested help topic '{}' is unknown.".format(args.topic))
                 self.context.print("The following topics are known:")
--- src/controller/cli/show_command.py
+++ src/controller/cli/show_command.py
@@ -13,13 +13,13 @@ class ShowCommand(Command):
         super().__init__(context, "showctl")
 
     def configure_parser(self, parser):
-        subparsers = parser.add_subparsers(help="showctrl commands", dest="showaction")
-        commit_parser = subparsers.add_parser("commit", help="Commit the current show file state")
+        subparsers = parser.add_subparsers(help="showctl commands", dest="showaction")
+        commit_parser = subparsers.add_parser("commit", help="Commit the current show file state", exit_on_error=False)
         commit_parser.add_argument("--select-default-scene", help="Load the default scene after commit",
                                    action='store_true')
-        load_parser = subparsers.add_parser("load", help="Load a show file")
+        load_parser = subparsers.add_parser("load", help="Load a show file", exit_on_error=False)
         load_parser.add_argument("filename", help="The location of the .show file.")
-        scene_parser = subparsers.add_parser("select-scene", help="select a specific scene in the running show.")
+        scene_parser = subparsers.add_parser("select-scene", help="select a specific scene in the running show.", exit_on_error=False)
         scene_parser.add_argument("scene-id", help="The scene id to select", type=int)
 
     def execute(self, args) -> bool:
