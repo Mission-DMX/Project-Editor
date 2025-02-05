b992a137c15e38189dc97deae1fa201f6d9e6bd8
Doralitze <technikradio@icloud.com>
2025-02-05 12:54:02 +0100

    add: splash screen

--- src/main.py
+++ src/main.py
@@ -1,58 +1,82 @@
 # coding=utf-8
 """GUI and control elements for the software."""
-import atexit
-import json
-import logging.config
-import logging.handlers
-import pathlib
-import sys
-
-from PySide6 import QtWidgets
-from PySide6.QtCore import Qt
-
-from controller.cli.remote_control_port import RemoteCLIServer
-from controller.joystick.joystick_handling import JoystickHandler
-from model.final_globals import FinalGlobals
-from Style import Style
-from view.main_window import MainWindow
-
-logger = logging.getLogger("Project-Editor")
-
-
-def setup_logging():
-    """read logging from config file and set up the logger"""
-    config_file = pathlib.Path("../configs/logging.json")
-    with open(config_file, encoding="utf-8") as f_in:
-        config = json.load(f_in)
-
-    logging.config.dictConfig(config)
-    queue_handler = logging.getHandlerByName("queue_handler")
-    if queue_handler is not None:
-        queue_handler.listener.start()
-        atexit.register(queue_handler.listener.stop)
-
-
-def main():
-    """Startup"""
-    setup_logging()
-    logging.basicConfig(level="INFO")
-
-    app = QtWidgets.QApplication([])
-    app.setAttribute(Qt.ApplicationAttribute.AA_UseDesktopOpenGL)
-    width, height = app.primaryScreen().size().toTuple()
-    FinalGlobals.set_screen_width(width)
-    FinalGlobals.set_screen_height(height)
-    app.setStyleSheet(Style.APP)
-    JoystickHandler()
-    widget = MainWindow()
-    widget.showMaximized()
-
-    cli_server = RemoteCLIServer(widget.show_configuration, widget._fish_connector)
-    return_code = app.exec()
-    cli_server.stop()
-    sys.exit(return_code)
-
 
 if __name__ == "__main__":
+    from PySide6.QtWidgets import QApplication, QSplashScreen
+    app = QApplication([])
+    from PySide6.QtGui import QPixmap
+
+    splashscreen = QSplashScreen(QPixmap("resources/splash.png"))
+    splashscreen.show()
+    splashscreen.raise_()
+    app.processEvents()
+
+    version_string = "Error reading version."
+    with open("resources/version.txt", "r") as f:
+        try:
+            version_string = "Version: " + f.read().replace('\n', '').strip()
+        except:
+            pass
+    from PySide6.QtGui import QColor
+    from PySide6.QtCore import Qt
+    splashscreen.showMessage(version_string, alignment=Qt.AlignmentFlag.AlignCenter, color=QColor.fromRgb(125, 125, 125))
+    app.processEvents()
+
+    import atexit
+    import json
+    import logging.config
+    import logging.handlers
+    import pathlib
+    import sys
+
+    from controller.cli.remote_control_port import RemoteCLIServer
+    from controller.joystick.joystick_handling import JoystickHandler
+    from model.final_globals import FinalGlobals
+    from Style import Style
+    from view.main_window import MainWindow
+
+    logger = logging.getLogger("Project-Editor")
+
+
+    def setup_logging():
+        """read logging from config file and set up the logger"""
+        config_file = pathlib.Path("../configs/logging.json")
+        with open(config_file, encoding="utf-8") as f_in:
+            config = json.load(f_in)
+
+        logging.config.dictConfig(config)
+        queue_handler = logging.getHandlerByName("queue_handler")
+        if queue_handler is not None:
+            queue_handler.listener.start()
+            atexit.register(queue_handler.listener.stop)
+
+
+    def main(app: QApplication):
+        """Startup"""
+        setup_logging()
+        logging.basicConfig(level="INFO")
+
+        app.setAttribute(Qt.ApplicationAttribute.AA_UseDesktopOpenGL)
+
+        width, height = app.primaryScreen().size().toTuple()
+        FinalGlobals.set_screen_width(width)
+        FinalGlobals.set_screen_height(height)
+        app.setStyleSheet(Style.APP)
+        JoystickHandler()
+
+        # TODO we should parse the global application settings and recent project files here
+        # TODO show a dialog asking the user to create a new show file or open a recent one
+        # TODO pass the show file to be loaded as a parameter to the constructor of main window.
+        # TODO pass "" if a new one should be created
+        # TODO open a new MainWindow if the user clicks new from the window menu
+        widget = MainWindow()
+        widget.showMaximized()
+        splashscreen.finish(widget)
+
+        cli_server = RemoteCLIServer(widget.show_configuration, widget._fish_connector)
+        return_code = app.exec()
+        cli_server.stop()
+        sys.exit(return_code)
+
     # Only start if __main__
-    main()
+    main(app)
