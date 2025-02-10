2bf20fb74ebeb2f01064e20454e7ffc1888379c7
MKay <ma.kaussow@uni-luebeck.de>
2025-02-10 17:10:17 +0100

    isort test

--- test/manual_test_filter_config_widgets.py
+++ test/manual_test_filter_config_widgets.py
@@ -1,9 +1,8 @@
 from PySide6 import QtWidgets
 from PySide6.QtWidgets import QMainWindow, QVBoxLayout
 
-from Style import Style
 from model.control_desk import *
-from controller.network import NetworkManager
+from Style import Style
 from view.show_mode.node_editor_widgets import ColumnSelect
 
 if __name__ == "__main__":
