310d804a25c09fe8c3f64443cbaf727d5f39063f
Leon Dietrich <doralitze@chaotikum.org>
2025-04-01 15:40:48 +0200

    chg: sender list widget to custom one

--- src/view/action_setup_view/event_setup_widget.py
+++ src/view/action_setup_view/event_setup_widget.py
@@ -1,15 +1,49 @@
+from logging import getLogger
+
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QIcon
-from PySide6.QtWidgets import QListWidget, QScrollArea, QSplitter, QToolBar, QVBoxLayout, QWidget
+from PySide6.QtWidgets import QHBoxLayout, QLabel, QListWidget, QScrollArea, QSplitter, QToolBar, QVBoxLayout, QWidget
 
 from model import Broadcaster, events
 from view.show_mode.editor.show_browser.annotated_item import AnnotatedListWidgetItem
+
+logger = getLogger(__file__)
+_xtouch_gpio_icon = QIcon("resources/icons/eventsource-gpio.svg")
+_plain_icon = QIcon("resources/icons/eventsource-plain.svg")
+_keypad_icon = QIcon("resources/icons/eventsource-keypad.svg")
+_midi_icon = QIcon("resources/icons/eventsource-midi.svg")
+_midirtp_icon = QIcon("resources/icons/eventsource-midirtp.svg")
 
 
 class _SenderConfigurationWidget(QScrollArea):
     def __init__(self, parent: QWidget | None):
         super().__init__(parent=parent)
         # TODO implement
+
+
+class _SourceListWidget(QWidget):
+
+    def __init__(self, parent: QWidget, sender: events.EventSender):
+        super().__init__(parent=parent)
+        layout = QHBoxLayout()
+        text_layout = QVBoxLayout()
+        self._icon_label = QLabel(self)
+        self._icon_label.setMaximumWidth(66)
+        self._icon_label.setMinimumWidth(64)
+        if isinstance(sender, events.XtouchGPIOEventSender):
+            self._icon_label.setPixmap(_xtouch_gpio_icon.pixmap(64, 64))
+        else:
+            self._icon_label.setPixmap(_plain_icon.pixmap(64, 64))
+        layout.addWidget(self._icon_label)
+        self._name_label = QLabel(self)
+        self._name_label.setText(sender.name)
+        text_layout.addWidget(self._name_label)
+        self._id_label = QLabel(self)
+        self._id_label.setText(f"Index: {sender.index_on_fish}")
+        self._id_label.setEnabled(False)
+        text_layout.addWidget(self._id_label)
+        layout.addLayout(text_layout)
+        self.setLayout(layout)
 
 
 class EventSetupWidget(QSplitter):
@@ -44,13 +78,13 @@ class EventSetupWidget(QSplitter):
 
     def _update_sender_list(self):
         self._sender_list.clear()
-        # TODO implement repopulation
         for sender in events.get_all_senders():
             item = AnnotatedListWidgetItem(self._sender_list)
-            item.setText(f"[{sender.index_on_fish}] {sender.name}")
+            item_widget = _SourceListWidget(self._sender_list, sender)
+            item.setSizeHint(item_widget.sizeHint())
             item.annotated_data = sender
             self._sender_list.addItem(item)
-            # TODO set better item based on https://stackoverflow.com/questions/25187444/pyqt-qlistwidget-custom-items
+            self._sender_list.setItemWidget(item, item_widget)
 
     def _sender_selected(self):
         pass  # TODO
