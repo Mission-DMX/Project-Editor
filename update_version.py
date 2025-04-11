1fc05bf05bad8dea7d110b82c7db8c556374f712
MKay <ma.kaussow@uni-luebeck.de>
2025-04-11 02:40:55 +0200

    build with additional PR

--- update_version.py
+++ update_version.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 """Update Version in GitHub action"""
+import sys
+
 import tomlkit
 
 # Load pyproject.toml
@@ -10,12 +12,14 @@ with open('pyproject.toml', 'r', encoding="UTF-8") as f:
 version = data['project']['version']
 major, minor, patch = map(int, version.split('.'))
 
-# Increment the patch version
-new_version = f'{major}.{minor}.{patch + 1}'
+if sys.argv[1] == "info":
+    print(f'{major}.{minor}.{patch}')
+else:
+    new_version = f'{major}.{minor}.{patch + 1}'
 
-# Update the version in the TOML file
-data['project']['version'] = new_version
-with open('pyproject.toml', 'w', encoding="UTF-8") as f:
-    tomlkit.dump(data, f)
+    # Update the version in the TOML file
+    data['project']['version'] = new_version
+    with open('pyproject.toml', 'w', encoding="UTF-8") as f:
+        tomlkit.dump(data, f)
 
-print(f'v{new_version}')
+    print(f'v{new_version}')
