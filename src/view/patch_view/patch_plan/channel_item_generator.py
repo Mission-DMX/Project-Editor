5ca52a2e6827e52a7e9956a41a4101f99be48141
CorsCodini <45790567+CorsCodini@users.noreply.github.com>
2025-06-03 15:27:12 +0200

    chg: Naming convention

--- src/view/patch_view/patch_plan/channel_item_generator.py
+++ src/view/patch_view/patch_plan/channel_item_generator.py
@@ -2,30 +2,30 @@
 """ item of the Patching """
 from PySide6.QtGui import QColorConstants, QFont, QPainter, QPixmap
 
-_width = 100
-_height = 100
+_WIDTH = 100
+_HEIGHT = 100
 
 
 def item_width():
     """width of the item"""
-    return _width
+    return _WIDTH
 
 
 def item_height():
     """height of the item"""
-    return _height
+    return _HEIGHT
 
 
 def create_item(number, color=QColorConstants.White) -> QPixmap:
     """creates a pixmap of the item"""
-    pixmap = QPixmap(_width, _height)
+    pixmap = QPixmap(_WIDTH, _HEIGHT)
     pixmap.fill(color)
 
     painter = QPainter(pixmap)
     painter.setRenderHint(QPainter.RenderHint.Antialiasing)
 
     painter.setPen(QColorConstants.Black)
-    painter.drawRect(0, 0, _width - 1, _height - 1)
+    painter.drawRect(0, 0, _WIDTH - 1, _HEIGHT - 1)
 
     font = QFont("Arial", 12)
     painter.setFont(font)
