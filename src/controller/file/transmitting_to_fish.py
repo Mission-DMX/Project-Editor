3fd18bdfe864255f8974c36c81f18990e5df7ed0
CorsCodini <45790567+CorsCodini@users.noreply.github.com>
2025-06-14 01:45:07 +0200

    add: Doku

--- src/controller/file/transmitting_to_fish.py
+++ src/controller/file/transmitting_to_fish.py
@@ -1,11 +1,13 @@
 # coding=utf-8
+"""Transmitting data as XML to Fish"""
+
 from controller.file.serializing.general_serialization import create_xml
 from controller.utils.process_notifications import get_process_notifier
 from model import BoardConfiguration
 
 
 def transmit_to_fish(show: BoardConfiguration, goto_default_scene: bool = True) -> bool:
-    """Send the current board configuration as a xml file to fish"""
+    """Send the current board configuration as an XML file to fish"""
     # TODO this could be an external QThread
     pn = get_process_notifier("Uploading Show to Fish", len(show.scenes))
     pn.current_step_description = "Checking for unlinked fader bank sets."
