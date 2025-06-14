17114a179f63ecce13b3f79b4460b15d23b838ab
Leon Dietrich <doralitze@chaotikum.org>
2025-06-14 17:01:48 +0200

    fix: upstart in non-dev environment

--- src/model/color_hsi.py
+++ src/model/color_hsi.py
@@ -1,15 +1,19 @@
 # coding=utf-8
 """Color in HSI Form"""
 import colorsys
+from typing import TYPE_CHECKING
 
-from pydantic import confloat
 from PySide6.QtGui import QColor
+
+
+if TYPE_CHECKING:
+    from pydantic import confloat
 
 
 class ColorHSI:
     """Color Definition"""
 
-    def __init__(self, hue: confloat(ge=0, le=360), saturation: confloat(ge=0, le=1), intensity: confloat(ge=0, le=1)):
+    def __init__(self, hue: "confloat(ge=0, le=360)", saturation: "confloat(ge=0, le=1)", intensity: "confloat(ge=0, le=1)"):
         """ HSI Color
         Args:
             hue: color itself in the form of an angle between [0,360] degrees
@@ -17,9 +21,9 @@ class ColorHSI:
             intensity: in range of [0,1] where 0 is black and 1 is white
 
         """
-        self._hue: confloat(ge=0, le=360) = hue
-        self._saturation: confloat(ge=0, le=1) = saturation
-        self._intensity: confloat(ge=0, le=1) = intensity
+        self._hue: "confloat(ge=0, le=360)" = hue
+        self._saturation: "confloat(ge=0, le=1)" = saturation
+        self._intensity: "confloat(ge=0, le=1)" = intensity
 
     @classmethod
     def from_filter_str(cls, filter_format: str):
@@ -49,12 +53,12 @@ class ColorHSI:
         return ColorHSI(hue, saturation, intensity)
 
     @property
-    def hue(self) -> confloat(ge=0, le=360):
+    def hue(self) -> "confloat(ge=0, le=360)":
         """color itself in the form of an angle between [0,360] degrees"""
         return self._hue
 
     @property
-    def saturation(self) -> confloat(ge=0, le=1):
+    def saturation(self) -> "confloat(ge=0, le=1)":
         """Saturation of the color.
 
         Float between (including) zero (100% white, 0% color) and 1 (0% white, 100% color)
@@ -62,7 +66,7 @@ class ColorHSI:
         return self._saturation
 
     @property
-    def intensity(self) -> confloat(ge=0, le=1):
+    def intensity(self) -> "confloat(ge=0, le=1)":
         """Perceived illuminance, float [0, 1]
         where 0 is black and 1 is white
         """
