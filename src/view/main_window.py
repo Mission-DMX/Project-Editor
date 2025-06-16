c4a27aefd20ab2e4e11607be147ce0ab0bd14f79
CorsCodini <45790567+CorsCodini@users.noreply.github.com>
2025-06-16 17:35:53 +0200

    disable theater_scene_wizard until rebuild

--- src/view/main_window.py
+++ src/view/main_window.py
@@ -178,10 +178,10 @@ class MainWindow(QtWidgets.QMainWindow):
                 ("&Undo", None, "Z"),  # TODO implement edit history
                 ("&Redo", None, "Shift+Z")
             ],
-            "Show": [
-                ("Scene Wizard", self._open_scene_setup_wizard, None)
-                # TODO link wizard that creates a theater scene based on patched fixtures
-            ],
+            #"Show": [
+            #    ("Scene Wizard", self._open_scene_setup_wizard, None)
+            #    # TODO link wizard that creates a theater scene based on patched fixtures
+            #],
             "Help": [
                 ("&About", self._open_about_window, None)
             ]
