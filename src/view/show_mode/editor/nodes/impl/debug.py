78207900affc8f332e1267662ac5a2ca0855d05d
CorsCodini <45790567+CorsCodini@users.noreply.github.com>
2025-07-09 21:19:07 +0200

    chg: to snake case

--- src/view/show_mode/editor/nodes/impl/debug.py
+++ src/view/show_mode/editor/nodes/impl/debug.py
@@ -17,7 +17,7 @@ class Debug8BitNode(DebugNode):
     """Filter to debug an 8 bit value.
     TODO implement visualization
     """
-    nodeName = "8 Bit Filter (Debug)"
+    node_name = "8 Bit Filter (Debug)"
 
     def __init__(self, model: Filter, name: str) -> None:
         super().__init__(model=model, filter_type=FilterTypeEnumeration.FILTER_DEBUG_OUTPUT_8BIT, name=name)
@@ -29,7 +29,7 @@ class Debug16BitNode(DebugNode):
     """Filter to debug a 16 bit value.
     TODO implement visualization
     """
-    nodeName = "16 Bit Filter (Debug)"
+    node_name = "16 Bit Filter (Debug)"
 
     def __init__(self, model: Filter, name: str) -> None:
         super().__init__(model=model, filter_type=FilterTypeEnumeration.FILTER_DEBUG_OUTPUT_16BIT, name=name)
@@ -41,7 +41,7 @@ class DebugFloatNode(DebugNode):
     """Filter to debug a float/double value.
     TODO implement visualization
     """
-    nodeName = "Float Filter (Debug)"
+    node_name = "Float Filter (Debug)"
 
     def __init__(self, model: Filter, name: str) -> None:
         super().__init__(model=model, filter_type=FilterTypeEnumeration.FILTER_DEBUG_OUTPUT_FLOAT, name=name)
@@ -53,7 +53,7 @@ class DebugColorNode(DebugNode):
     """Filter to debug a color value.
     TODO implement visualization
     """
-    nodeName = "Color Filter (Debug)"
+    node_name = "Color Filter (Debug)"
 
     def __init__(self, model: Filter, name: str) -> None:
         super().__init__(model=model, filter_type=FilterTypeEnumeration.FILTER_DEBUG_OUTPUT_COLOR, name=name)
@@ -65,7 +65,7 @@ class DebugRemote8BitNode(DebugNode):
     """Filter to debug an 8 bit value.
     TODO implement visualization
     """
-    nodeName = "8 Bit Filter (Debug, Remote)"
+    node_name = "8 Bit Filter (Debug, Remote)"
 
     def __init__(self, model: Filter, name: str) -> None:
         super().__init__(model=model, filter_type=FilterTypeEnumeration.FILTER_REMOTE_DEBUG_8BIT, name=name)
@@ -77,7 +77,7 @@ class DebugRemote16BitNode(DebugNode):
     """Filter to debug a 16 bit value.
     TODO implement visualization
     """
-    nodeName = "16 Bit Filter (Debug, Remote)"
+    node_name = "16 Bit Filter (Debug, Remote)"
 
     def __init__(self, model: Filter, name: str) -> None:
         super().__init__(model=model, filter_type=FilterTypeEnumeration.FILTER_REMOTE_DEBUG_16BIT, name=name)
@@ -89,7 +89,7 @@ class DebugRemoteFloatNode(DebugNode):
     """Filter to debug a float/double value.
     TODO implement visualization
     """
-    nodeName = "Float Filter (Debug, Remote)"
+    node_name = "Float Filter (Debug, Remote)"
 
     def __init__(self, model: Filter, name: str) -> None:
         super().__init__(model=model, filter_type=FilterTypeEnumeration.FILTER_REMOTE_DEBUG_FLOAT, name=name)
@@ -101,7 +101,7 @@ class DebugRemoteColorNode(DebugNode):
     """Filter to debug a color value.
     TODO implement visualization
     """
-    nodeName = "Color Filter (Debug, Remote)"
+    node_name = "Color Filter (Debug, Remote)"
 
     def __init__(self, model: Filter, name: str) -> None:
         super().__init__(model=model, filter_type=FilterTypeEnumeration.FILTER_REMOTE_DEBUG_PIXEL, name=name)
