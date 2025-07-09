616b6a4f93c1b3fd95f60a4154d361f9c26cfe79
CorsCodini <45790567+CorsCodini@users.noreply.github.com>
2025-07-09 20:47:43 +0200

    chg: SettingsTab to settings_tab

--- src/view/show_mode/show_ui_widgets/autotracker/AutoTrackDialogWidget.py
+++ src/view/show_mode/show_ui_widgets/autotracker/AutoTrackDialogWidget.py
@@ -10,7 +10,7 @@ from view.show_mode.show_ui_widgets.autotracker.GuiTab import GuiTab
 
 # from view.show_mode.editor.show_ui_widgets.autotracker.DetectionTab import DetectionTab
 from view.show_mode.show_ui_widgets.autotracker.LightSetupTab import LightSetupTab
-from view.show_mode.show_ui_widgets.autotracker.SettingsTab import SettingsTab
+from view.show_mode.show_ui_widgets.autotracker.settings_tab import SettingsTab
 from view.show_mode.show_ui_widgets.autotracker.sources_tab import SourcesTab
 
 if TYPE_CHECKING:
