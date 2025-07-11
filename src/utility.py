765c8635b877cc8a65e0225410829ed65561a339
CorsCodini <45790567+CorsCodini@users.noreply.github.com>
2025-07-11 02:05:23 +0200

    chg: doku

--- src/utility.py
+++ src/utility.py
@@ -4,6 +4,6 @@ import sys
 
 
 def resource_path(relative_path: str) -> str:
-    """ Gibt den absoluten Pfad zur Ressource zurück – funktioniert im Skript und im PyInstaller-Executable """
+    """ Gibt den absoluten Pfad zur Ressource zurück. Funktioniert im Skript und im PyInstaller-Executable """
     base_path = getattr(sys, "_MEIPASS", os.path.abspath("."))
     return os.path.join(base_path, relative_path)
