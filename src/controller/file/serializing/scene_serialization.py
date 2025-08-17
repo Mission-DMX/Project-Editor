76b62a3bf50a7a0baeef316a15aeeef6735490e6
CorsCodini <45790567+CorsCodini@users.noreply.github.com>
2025-08-17 12:53:59 +0200

    Doku

--- src/controller/file/serializing/scene_serialization.py
+++ src/controller/file/serializing/scene_serialization.py
@@ -1,4 +1,5 @@
 """Serialization of Scenes to XML."""
+
 import xml.etree.ElementTree as ET
 
 from controller.file.serializing.bankset_config_serialization import _create_scene_bankset
@@ -12,18 +13,23 @@ from model import Scene, UIPage
 from model.scene import FilterPage
 
 
-def _add_filter_page_to_element(scene_element: ET.Element, page: FilterPage,
-                                parent_page: FilterPage | None) -> None:
-    """Writes the filter pages of a scene or parent page.
+def _add_filter_page_to_element(scene_element: ET.Element, page: FilterPage, parent_page: FilterPage | None) -> None:
+    """Write the filter pages of a scene or parent page.
 
-    :param scene_element: The XML element to write to
-    :param page: The filter page to serialize
-    :param parent_page: This needs to be the parent page of the one to serialize (if any, otherwise pass None)
+    Args:
+        scene_element: The XML element to write to.
+        page: The filter page to serialize.
+        parent_page: The parent page of the one to serialize, or None if not applicable.
+
     """
-    item = ET.SubElement(scene_element, "filterpage", attrib={
-        "name": page.name,
-        "parent": parent_page.name if parent_page else "",
-    })
+    item = ET.SubElement(
+        scene_element,
+        "filterpage",
+        attrib={
+            "name": page.name,
+            "parent": parent_page.name if parent_page else "",
+        },
+    )
     for f in page.filters:
         filter_id_item = ET.SubElement(item, "filterid", attrib={})
         filter_id_item.text = f.filter_id
@@ -34,36 +40,55 @@ def _add_filter_page_to_element(scene_element: ET.Element, page: FilterPage,
 def _add_ui_page_to_element(scene_element: ET.Element, ui_page: UIPage) -> None:
     """Add a UI page (the widgets one) to an existing scene element.
 
-    :param scene_element: The parent scene XML element
-    :param ui_page: The UI page to add
+    Args:
+        scene_element: The parent scene XML element.
+        ui_page: The UI page to add.
+
     """
-    page_element = ET.SubElement(scene_element, "uipage", attrib={
-        "title": str(ui_page.title),
-    })
+    page_element = ET.SubElement(
+        scene_element,
+        "uipage",
+        attrib={
+            "title": str(ui_page.title),
+        },
+    )
     from view.show_mode.show_ui_widgets import get_widget_key
+
     for widget in ui_page.widgets:
-        widget_element = ET.SubElement(page_element, "widget", attrib={
-            "posX": str(widget.position[0]),
-            "posY": str(widget.position[1]),
-            "sizeW": str(widget.size[0]),
-            "sizeH": str(widget.size[1]),
-            "filterID": ":".join(widget.filter_ids),
-            "variante": str(get_widget_key(widget) or ""),
-        })
+        widget_element = ET.SubElement(
+            page_element,
+            "widget",
+            attrib={
+                "posX": str(widget.position[0]),
+                "posY": str(widget.position[1]),
+                "sizeW": str(widget.size[0]),
+                "sizeH": str(widget.size[1]),
+                "filterID": ":".join(widget.filter_ids),
+                "variante": str(get_widget_key(widget) or ""),
+            },
+        )
         for k, v in widget.configuration.items():
-            ET.SubElement(widget_element, "configurationEntry", attrib={
-                "name": str(k),
-                "value": str(v),
-            })
+            ET.SubElement(
+                widget_element,
+                "configurationEntry",
+                attrib={
+                    "name": str(k),
+                    "value": str(v),
+                },
+            )
 
 
-def generate_scene_xml_description(assemble_for_fish_loading: bool, root: ET.Element, scene: Scene,
-                                   pn: ProcessNotifier) -> None:
-    """Generates the DOM tree for a given scene.
+def generate_scene_xml_description(
+    assemble_for_fish_loading: bool, root: ET.Element, scene: Scene, pn: ProcessNotifier
+) -> None:
+    """Generate the DOM tree for a given scene.
 
-    :param assemble_for_fish_loading: Boolean that should be true if and only if the data is being transferred to fish
-    :param root: The DOM root
-    :param scene: The scene to generate the XML data for
+    Args:
+        assemble_for_fish_loading: Should be True if the data is being transferred to Fish.
+        root: The DOM root.
+        scene: The scene to generate the XML data for.
+        pn: The process notifier.
+
     """
     scene_element = _create_scene_element(scene=scene, parent=root)
     if scene.linked_bankset and not assemble_for_fish_loading:
@@ -71,8 +96,9 @@ def generate_scene_xml_description(assemble_for_fish_loading: bool, root: ET.Ele
     pn.total_step_count += len(scene.filters)
     om = SceneOptimizerModule(assemble_for_fish_loading)
     for filter_ in scene.filters:
-        _create_filter_element_for_fish(filter_=filter_, parent=scene_element, for_fish=assemble_for_fish_loading,
-                                        om=om)
+        _create_filter_element_for_fish(
+            filter_=filter_, parent=scene_element, for_fish=assemble_for_fish_loading, om=om
+        )
         pn.current_step_number += 1
     create_channel_mappings_for_filter_set_for_fish(assemble_for_fish_loading, om, scene_element)
     if not assemble_for_fish_loading:
@@ -84,16 +110,22 @@ def generate_scene_xml_description(assemble_for_fish_loading: bool, root: ET.Ele
 
 
 def _create_scene_element(scene: Scene, parent: ET.Element) -> ET.Element:
-    """Creates an XML element of a type scene.
+    """Create an XML element of type scene.
 
+    Examples:
     <scene human_readable_name="name" id="0">
       ...
     </scene>
+
     """
-    se = ET.SubElement(parent, "scene", attrib={
-        "id": str(scene.scene_id),
-        "human_readable_name": str(scene.human_readable_name),
-    })
+    se = ET.SubElement(
+        parent,
+        "scene",
+        attrib={
+            "id": str(scene.scene_id),
+            "human_readable_name": str(scene.human_readable_name),
+        },
+    )
     if scene.linked_bankset:
         se.attrib["linkedBankset"] = str(scene.linked_bankset.id)
     return se
