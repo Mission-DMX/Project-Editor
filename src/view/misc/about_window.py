fb7374afcef70849d19fd283b6280c3fe24c8366
CorsCodini <45790567+CorsCodini@users.noreply.github.com>
2025-08-19 18:49:03 +0200

    Doku

--- src/view/misc/about_window.py
+++ src/view/misc/about_window.py
@@ -1,4 +1,4 @@
-"""This file provides the about-dialog"""
+"""About Window."""
 
 import os.path
 from logging import getLogger
@@ -17,11 +17,16 @@ with open(resource_path(os.path.join("resources", "pyproject.toml")), "r", encod
 
 
 def read_entire_file_as_str(file_path: str) -> str:
-    """Read the content of a text file and return the content as-is.
-    This method does not throw IO errors but may return a debug hint and log the issue in case something goes wrong.
+    """Read the content of a text file and return it as-is.
 
-    :param file_path: The path to the file to load
-    :returns: The content of the loaded file or a hint that something went wrong.
+    This method does not throw IO errors but may return a debug hint and log the issue if something goes wrong.
+
+    Args:
+        file_path: The path to the file to load.
+
+    Returns:
+        The content of the loaded file, or a hint that something went wrong.
+
     """
     # TODO in toml
     try:
@@ -38,11 +43,10 @@ CONTRIBUTORS_STR = html2text(read_entire_file_as_str(resource_path(os.path.join(
 
 
 class AboutWindow(QMessageBox):
-    """
-    This window displays the credits.
-    """
+    """About Window (credits)."""
 
     def __init__(self, parent: QWidget) -> None:
+        """About Window (credits)."""
         super().__init__(
             QMessageBox.Icon.Information, "<b>About</b>", f"<i>MissionDMX</i> - Version {VERSION_STR}", parent=parent
         )
