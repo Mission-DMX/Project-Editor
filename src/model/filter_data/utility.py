92b1f7872aa98b4f8317140ca84ea4cfd7633986
CorsCodini <45790567+CorsCodini@users.noreply.github.com>
2025-08-29 20:49:17 +0200

    Doku

--- src/model/filter_data/utility.py
+++ src/model/filter_data/utility.py
@@ -1,7 +1,4 @@
-"""Filter model utility methods.
-
-format_seconds -- Method to get duration in seconds as human readable string.
-"""
+"""Filter model utility methods."""
 
 import time
 
@@ -9,9 +6,11 @@ import time
 def format_seconds(seconds: float) -> str:
     """Format a given number of seconds into a time.
 
-    :param seconds: Number of seconds to format.
-    :type seconds: float
-    :returns: Formatted time as hours, minutes and seconds.
+    Args:
+        seconds: Number of seconds to format.
+
+    Returns: Formatted time as hours, minutes and seconds.
+
     """
     millis = int((seconds * 1000) % 1000)
     return f"{time.strftime('%H:%M:%S', time.gmtime(int(seconds)))}.{millis:0>3}"
