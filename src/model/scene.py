81cd1cf4de8cc87166c930361e25779525348077
CorsCodini <45790567+CorsCodini@users.noreply.github.com>
2025-08-29 21:05:17 +0200

    Doku

--- src/model/scene.py
+++ src/model/scene.py
@@ -1,8 +1,8 @@
-"""Scene module"""
+"""Scene module."""
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, override
 
 if TYPE_CHECKING:
     from .board_configuration import BoardConfiguration
@@ -12,9 +12,10 @@ if TYPE_CHECKING:
 
 
 class FilterPage:
-    """Filter page in a Scene"""
+    """Filter page in a Scene."""
 
     def __init__(self, parent: Scene) -> None:
+        """Filter page in a Scene."""
         self._filters: list[Filter] = []
         self._child_pages: list[FilterPage] = []
         self._parent_scene: Scene = parent
@@ -22,7 +23,7 @@ class FilterPage:
 
     @property
     def name(self) -> str:
-        """Name"""
+        """Name."""
         return self._name
 
     @name.setter
@@ -31,21 +32,21 @@ class FilterPage:
 
     @property
     def filters(self) -> list[Filter]:
-        """List of filters"""
+        """List of filters."""
         return self._filters
 
     @property
     def child_pages(self) -> list[FilterPage]:
-        """List of child pages"""
+        """List of child pages."""
         return self._child_pages
 
     @property
     def parent_scene(self) -> Scene:
-        """Parent scene"""
+        """Parent scene."""
         return self._parent_scene
 
-    def copy(self, new_scene: Scene = None) -> Scene:
-        """Copy of a filter page"""
+    def copy(self, new_scene: Scene = None) -> FilterPage:
+        """Copy of a filter page."""
         if not new_scene:
             new_fp = FilterPage(self._parent_scene)
             # Internal copying should not copy filters as they are unique within Scene
@@ -65,6 +66,7 @@ class Scene:
     """Scene for a show file."""
 
     def __init__(self, scene_id: int, human_readable_name: str, board_configuration: BoardConfiguration) -> None:
+        """Scene for a show file."""
         self._scene_id: int = scene_id
         self._human_readable_name: str = human_readable_name
         self._board_configuration: BoardConfiguration = board_configuration
@@ -76,32 +78,36 @@ class Scene:
 
     @property
     def scene_id(self) -> int:
-        """The unique id of the scene"""
+        """The unique id of the scene."""
         return self._scene_id
 
     @property
     def human_readable_name(self) -> str:
-        """The human-readable name of the scene displayed by the ui"""
+        """The human-readable name of the scene displayed by the ui."""
         return self._human_readable_name
 
     @human_readable_name.setter
     def human_readable_name(self, human_readable_name: str) -> None:
-        """Sets the human-readable name of the scene displayed by the ui"""
         self._human_readable_name = human_readable_name
 
     @property
     def board_configuration(self) -> BoardConfiguration:
-        """The board configuration the scene is part of"""
+        """The board configuration the scene is part of."""
         return self._board_configuration
 
     @property
     def filters(self) -> list[Filter]:
-        """The filters of the scene. Warning: do not use this to call append on it. Use scene.append_filter instead."""
+        """The filters of the scene.
+
+        Warning:
+            do not use this to call append on it.  Use scene.append_filter instead.
+
+        """
         return self._filters
 
     @property
     def pages(self) -> list[FilterPage]:
-        """Returns the associated list of filter pages"""
+        """Returns the associated list of filter pages."""
         if len(self._filter_pages) == 0:
             default_page = FilterPage(self)
             default_page.name = "default"
@@ -111,12 +117,12 @@ class Scene:
         return self._filter_pages
 
     def insert_filterpage(self, fp: FilterPage) -> None:
-        """Add a filterpage to the scene"""
+        """Add a filterpage to the scene."""
         self._filter_pages.append(fp)
 
     @property
     def linked_bankset(self) -> BankSet | None:
-        """associated_bankset"""
+        """Associated_bankset."""
         return self._associated_bankset
 
     @linked_bankset.setter
@@ -127,16 +133,20 @@ class Scene:
 
     @property
     def ui_pages(self) -> list[UIPage]:
-        """List of UIPages"""
+        """List of UIPages."""
         return self._ui_pages
 
+    @override
     def __str__(self) -> str:
         if self.human_readable_name:
             return self.human_readable_name
         return str(self._scene_id)
 
     def ensure_bankset(self) -> bool:
-        """This method makes sure that the bank set associated with this scene gets applied to fish."""
+        """Apply the bank set associated with this scene to fish.
+
+        This method ensures that the correct bank set is activated in fish for this scene.
+        """
         if self._associated_bankset is None:
             from .control_desk import BankSet
 
@@ -147,9 +157,11 @@ class Scene:
         return True
 
     def copy(self, existing_scenes: list[Scene]) -> Scene:
-        """This method returns a copy of the scene, jet containing a new unique ID.
+        """Return a copy of the scene, jet containing a new unique ID.
 
-        existing_scenes: A list of scenes to check the new id against.
+        Args:
+            existing_scenes: A list of scenes to check the new id against.
+
         """
         chosen_id = len(existing_scenes)
         id_check_passed: bool = False
@@ -175,7 +187,7 @@ class Scene:
         return scene
 
     def get_filter_by_id(self, fid: str) -> Filter | None:
-        """Get filter by filter ID"""
+        """Get filter by filter ID."""
         f = self._filter_index.get(fid)
         if f:
             return f
@@ -186,9 +198,13 @@ class Scene:
         return None
 
     def ensure_name_uniqueness(self, name_to_try: str) -> str:
-        """This method checks the provided name for uniqueness within this scene.
-        :param name_to_try: The name to check for uniqueness
-        :returns: A minimal modified version of the provided name that ensures uniqueness.
+        """Check the provided name for uniqueness within this scene.
+
+        Args:
+            name_to_try: The name to check for uniqueness
+
+        Returns: A minimal modified version of the provided name that ensures uniqueness.
+
         """
         while self.get_filter_by_id(name_to_try):
             name_appendix = ""
@@ -202,9 +218,12 @@ class Scene:
 
     def append_filter(self, f: Filter, filter_page_index: int = -1) -> None:
         """Insert a filter in the scene.
-        :param f: The filter to add
-        :param filter_page_index: The index of the filter page to add it to.
+
+        Args:
+            f: The filter to add
+            filter_page_index: The index of the filter page to add it to.
                                   -1 indicates that this step should be skipped.
+
         """
         if f.scene and f.scene != self:
             raise Exception(f"This filter ({f.filter_id}) is already added to a scene other than this one")
@@ -234,9 +253,12 @@ class Scene:
             remove_filter_from_page(p)
 
     def notify_about_filter_rename_action(self, sender: Filter, old_id: str) -> None:
-        """This method checks connections of a filter which should be renamed and updates them accordingly.
-        :param sender: The filter that was renamed
-        :param old_id: The id which this filter was known as before.
+        """Check connections of a filter which should be renamed and updates them accordingly.
+
+        Args:
+            sender: The filter that was renamed
+            old_id: The id which this filter was known as before.
+
         """
         if self._filter_index.get(old_id):
             self._filter_index.pop(old_id)
