961c44015fb5669b2c1068c373a3406d63b28c7a
CorsCodini <45790567+CorsCodini@users.noreply.github.com>
2025-08-29 21:19:46 +0200

    Doku

--- src/controller/autotrack/Calibration/MappingCalibration.py
+++ src/controller/autotrack/Calibration/MappingCalibration.py
@@ -1,12 +1,14 @@
 # coding=utf-8
+from typing import override
+
 import cv2
 import numpy as np
 
 
 def _parse_parameters(str_representation: str) -> list[tuple[tuple[int, int], tuple[int, int]]]:
     param_list = []
-    for p_str in str_representation.split(';'):
-        p_parts = p_str.split(',')
+    for p_str in str_representation.split(";"):
+        p_parts = p_str.split(",")
         for i in range(p_parts):
             p_parts[i] = int(p_parts[i])
         param_list.append(((p_parts[0], p_parts[1]), (p_parts[2], p_parts[3])))
@@ -14,7 +16,10 @@ def _parse_parameters(str_representation: str) -> list[tuple[tuple[int, int], tu
 
 
 class MappingCalibration:
+    """Calibration for auto track Camera."""
+
     def __init__(self, points: list[tuple[tuple[int, int], tuple[int, int]]] | str):
+        """Calibration for auto track Camera."""
         if isinstance(points, str):
             points = _parse_parameters(points)
         self._original_representation = points
@@ -28,11 +33,13 @@ class MappingCalibration:
         self.M, _ = cv2.findHomography(src_points, dst_points, method=cv2.RANSAC)
 
     def get_point(self, point: tuple[int, int]) -> tuple[int, int]:
-        """
-        This method transforms a point received as coordinates within the camera frame into Pan/Tilt coordinates for the moving head.
+        """Transform a point received as coordinates within the camera frame into Pan/Tilt coordinates for the moving head.
 
-        :param point: The position of the person relative to the frame
-        :returns: The position of the person expressed in moving head coordinates
+        Args:
+            point: The position of the person relative to the frame
+
+        Returns: The position of the person expressed in moving head coordinates
+
         """
         x, y = point
         pt = np.array([[x, y]], dtype=np.float32)
@@ -40,7 +47,9 @@ class MappingCalibration:
         c = (int(transformed_point[0][0][0]), int(transformed_point[0][0][1]))
         return c
 
+    @override
     def __str__(self):
         def generate_representation(p: tuple[tuple[int, int], tuple[int, int]]) -> str:
             return ",".join([str(p[0][0]), str(p[0][1]), str(p[1][0]), str(p[1][1])])
+
         return ";".join([generate_representation(p) for p in self._original_representation])
