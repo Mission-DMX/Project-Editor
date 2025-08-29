e647aea66b02b404295a325e83a124482ee7ddcf
CorsCodini <45790567+CorsCodini@users.noreply.github.com>
2025-08-29 20:57:31 +0200

    Beutification

--- src/controller/cli/show_command.py
+++ src/controller/cli/show_command.py
@@ -1,6 +1,5 @@
-"""
-This file contains a command implementation to control show files, including their running state on fish.
-"""
+"""This file contains a command implementation to control show files, including their running state on fish."""
+
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
@@ -17,8 +16,7 @@ if TYPE_CHECKING:
 
 
 class ShowCommand(Command):
-    """
-    Control the loadding, saving and transmitting of a show file. Furthermore, this command selection enables the
+    """Control the loadding, saving and transmitting of a show file. Furthermore, this command selection enables the
     control over a running show on fish.
     """
 
@@ -26,24 +24,26 @@ class ShowCommand(Command):
         super().__init__(context, "showctl")
 
     def configure_parser(self, parser: argparse.ArgumentParser) -> None:
-        """
-        Configure the sub parser of the CLI.
+        """Configure the sub parser of the CLI.
 
         :param parser: The argparse subparser to configure.
         """
         subparsers = parser.add_subparsers(help="showctl commands", dest="showaction")
-        commit_parser: ArgumentParser = subparsers.add_parser("commit", help="Commit the current show file state",
-                                                              exit_on_error=False)
-        commit_parser.add_argument("--select-default-scene", help="Load the default scene after commit",
-                                   action="store_true")
+        commit_parser: ArgumentParser = subparsers.add_parser(
+            "commit", help="Commit the current show file state", exit_on_error=False
+        )
+        commit_parser.add_argument(
+            "--select-default-scene", help="Load the default scene after commit", action="store_true"
+        )
         load_parser: ArgumentParser = subparsers.add_parser("load", help="Load a show file", exit_on_error=False)
         load_parser.add_argument("filename", help="The location of the .show file.")
-        scene_parser: ArgumentParser = subparsers.add_parser("select-scene",
-                                                             help="select a specific scene in the running show.",
-                                                             exit_on_error=False)
+        scene_parser: ArgumentParser = subparsers.add_parser(
+            "select-scene", help="select a specific scene in the running show.", exit_on_error=False
+        )
         scene_parser.add_argument("sceneid", help="The scene id to select", type=int)
-        filtercmd_parser: ArgumentParser = subparsers.add_parser("filtermsg", help="Send an update to a filter",
-                                                                 exit_on_error=False)
+        filtercmd_parser: ArgumentParser = subparsers.add_parser(
+            "filtermsg", help="Send an update to a filter", exit_on_error=False
+        )
         filtercmd_parser.add_argument("sceneid", help="The scene id to select", type=int)
         filtercmd_parser.add_argument("filterid", help="The filter to update")
         filtercmd_parser.add_argument("parameterkey", help="The key of the parameter to update")
@@ -63,8 +63,8 @@ class ShowCommand(Command):
                 self.context.network_manager.enter_scene(scene, push_direct=False)
                 return True
             case "filtermsg":
-                self.context.network_manager.send_gui_update_to_fish(args.sceneid, args.filterid,
-                                                                     args.parameterkey, args.parametervalue,
-                                                                     enque=True)
+                self.context.network_manager.send_gui_update_to_fish(
+                    args.sceneid, args.filterid, args.parameterkey, args.parametervalue, enque=True
+                )
                 return True
         return False
