436805f3c1f84c15308c9204838550bcd990801b
CorsCodini <45790567+CorsCodini@users.noreply.github.com>
2025-08-29 21:30:15 +0200

    mv: Style in style.py

--- src/view/show_mode/editor/node_editor_widgets/sequencer_editor/channel_label.py
+++ src/view/show_mode/editor/node_editor_widgets/sequencer_editor/channel_label.py
@@ -7,6 +7,7 @@ from typing import TYPE_CHECKING
 from PySide6.QtCore import Qt
 from PySide6.QtWidgets import QHBoxLayout, QLabel, QWidget
 
+import style
 from model import DataType
 
 if TYPE_CHECKING:
@@ -15,34 +16,6 @@ if TYPE_CHECKING:
 
 class ChannelLabel(QWidget):
     """Label to represent a sequencer channel in a QListWidget."""
-
-    _CHANNEL_STYLE_8BIT = """
-    background-color: #202020;
-    color: #DDDDDD;
-    border-radius: 5px;
-    padding: 3px;
-    """
-
-    _CHANNEL_STYLE_16BIT = """
-    background-color: #202020;
-    color: #EEEEEE;
-    border-radius: 5px;
-    padding: 3px;
-    """
-
-    _CHANNEL_STYLE_FLOAT = """
-    background-color: #202020;
-    color: #FFFFFF;
-    border-radius: 5px;
-    padding: 3px;
-    """
-
-    _CHANNEL_STYLE_COLOR = """
-    background-color: #202020;
-    color: #10AA10;
-    border-radius: 5px;
-    padding: 3px;
-    """
 
     def __init__(self, channel: SequencerChannel, parent: QWidget | None = None) -> None:
         """Initialize the channel label using the given channel."""
@@ -53,15 +26,15 @@ class ChannelLabel(QWidget):
         dtype_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         match channel.data_type:
             case DataType.DT_DOUBLE:
-                dtype_label.setStyleSheet(self._CHANNEL_STYLE_FLOAT)
+                dtype_label.setStyleSheet(style.CHANNEL_STYLE_FLOAT)
             case DataType.DT_COLOR:
-                dtype_label.setStyleSheet(self._CHANNEL_STYLE_COLOR)
+                dtype_label.setStyleSheet(style.CHANNEL_STYLE_COLOR)
             case DataType.DT_16_BIT:
-                dtype_label.setStyleSheet(self._CHANNEL_STYLE_16BIT)
+                dtype_label.setStyleSheet(style.CHANNEL_STYLE_16BIT)
             case DataType.DT_8_BIT:
-                dtype_label.setStyleSheet(self._CHANNEL_STYLE_8BIT)
+                dtype_label.setStyleSheet(style.CHANNEL_STYLE_8BIT)
             case _:
-                dtype_label.setStyleSheet(self._CHANNEL_STYLE_8BIT)
+                dtype_label.setStyleSheet(style.CHANNEL_STYLE_8BIT)
         dtype_label.setFixedWidth(64)
         layout.addWidget(dtype_label)
         layout.addSpacing(10)
