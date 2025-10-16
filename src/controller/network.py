fbcf59358c09fbee90e7c3c0bdf788568625060e
Joell <joell.preu@gmail.com>
2025-10-16 13:13:53 +0200

    a

--- src/controller/network.py
+++ src/controller/network.py
@@ -79,7 +79,7 @@ class NetworkManager(QtCore.QObject, metaclass=QObjectSingletonMeta):
             lambda: self.update_state(proto.RealTimeControl_pb2.RunMode.RM_DIRECT),
         )
 
-        self._broadcaster.transmitting_show_file.connect(self.transmit_show_file)
+        #self._broadcaster.transmitting_show_file.connect(self.transmit_show_file)
         self._broadcaster.change_active_scene.connect(self.enter_scene)
 
         x_touch.XTouchMessages(self._broadcaster, self.button_msg_to_x_touch)
