00b6fdecc044168725f7f2d9d37c567153257daf
Leon Dietrich <doralitze@chaotikum.org>
2025-10-27 15:52:04 +0100

    add: docu to pan_tilt_constant.py

--- src/model/virtual_filters/pan_tilt_constant.py
+++ src/model/virtual_filters/pan_tilt_constant.py
@@ -1,3 +1,4 @@
+"""Virtual filter providing combined constants for pan and tilt."""
 from __future__ import annotations
 
 import typing
@@ -15,8 +16,10 @@ if typing.TYPE_CHECKING:
 
 
 class PanTiltConstantFilter(VirtualFilter):
+    """Virtual filter providing combined constants for pan and tilt."""
 
     def __init__(self, scene: Scene, filter_id: str, pos: tuple[int] | None = None) -> None:
+        """Initializes the virtual filter."""
         super().__init__(scene, filter_id, FilterTypeEnumeration.VFILTER_POSITION_CONSTANT, pos=pos)
         self._pan: float = 0.8
         self._tilt: float = 0.8
@@ -31,9 +34,10 @@ class PanTiltConstantFilter(VirtualFilter):
         # Todo: maybe use timer in broadcaster
         self._timer = QTimer()
         self._timer.setInterval(50)
-        self._timer.timeout.connect(self.update_time_passed)
+        self._timer.timeout.connect(self._update_time_passed)
         self.observer: dict[PanTiltConstantControlUIWidget, Callable[[], None]] = {}
 
+    @typing.override
     def resolve_output_port_id(self, virtual_port_id: str) -> str | None:
         match virtual_port_id:
             case "pan8bit":
@@ -46,14 +50,15 @@ class PanTiltConstantFilter(VirtualFilter):
                 return f"{self.filter_id}_16bit_tilt:value"
         return None
 
+    @typing.override
     def instantiate_filters(self, filter_list: list[Filter]) -> None:
-        self.instantiate_16bit_constant_filter(filter_list, False)
-        self.instantiate_16bit_constant_filter(filter_list, True)
+        self._instantiate_16bit_constant_filter(filter_list, False)
+        self._instantiate_16bit_constant_filter(filter_list, True)
         if self.eight_bit_available:
-            self.instantiate_16bit_to_8bit_conversion_filter(filter_list, False)
-            self.instantiate_16bit_to_8bit_conversion_filter(filter_list, True)
+            self._instantiate_16bit_to_8bit_conversion_filter(filter_list, False)
+            self._instantiate_16bit_to_8bit_conversion_filter(filter_list, True)
 
-    def instantiate_16bit_constant_filter(self, filter_list: list[Filter], tilt: bool) -> None:
+    def _instantiate_16bit_constant_filter(self, filter_list: list[Filter], tilt: bool) -> None:
         filter_ = Filter(
             filter_id=f"{self.filter_id}_16bit_{'tilt' if tilt else 'pan'}",
             filter_type=FilterTypeEnumeration.FILTER_CONSTANT_16_BIT,
@@ -69,7 +74,7 @@ class PanTiltConstantFilter(VirtualFilter):
         filter_._channel_links = {}
         filter_list.append(filter_)
 
-    def instantiate_16bit_to_8bit_conversion_filter(self, filter_list: list[Filter], tilt: bool) -> None:
+    def _instantiate_16bit_to_8bit_conversion_filter(self, filter_list: list[Filter], tilt: bool) -> None:
         filter_ = Filter(
             filter_id=f"{self.filter_id}_8bit_{'tilt' if tilt else 'pan'}",
             filter_type=FilterTypeEnumeration.FILTER_ADAPTER_16BIT_TO_DUAL_8BIT,
@@ -87,24 +92,31 @@ class PanTiltConstantFilter(VirtualFilter):
 
     @property
     def pan(self) -> float:
+        """Get or set the pan constant."""
         return self._pan
 
     @pan.setter
     def pan(self, pan: float) -> None:
         self._pan = pan
-        self.notify_observer()
+        self._notify_observer()
 
     @property
     def tilt(self) -> float:
+        """Get or set the tilt constant."""
         return self._tilt
 
     @tilt.setter
     def tilt(self, tilt: float) -> None:
         self._tilt = tilt
-        self.notify_observer()
+        self._notify_observer()
 
     @property
     def pan_delta(self) -> float:
+        """Get or set the pan delta.
+
+        The pan delta is the change applied per input movement. A larger delta results in faster changes.
+
+        """
         return self._pan_delta
 
     @pan_delta.setter
@@ -113,6 +125,11 @@ class PanTiltConstantFilter(VirtualFilter):
 
     @property
     def tilt_delta(self) -> float:
+        """Get or set the tilt delta.
+
+        The tilt delta is the change applied per input movement. A larger delta results in faster changes.
+
+        """
         return self._tilt_delta
 
     @tilt_delta.setter
@@ -120,6 +137,7 @@ class PanTiltConstantFilter(VirtualFilter):
         self._tilt_delta = tilt_delta
 
     def set_delta(self, delta: float, joystick: JoystickList, tilt: bool) -> None:
+        """Use this method to input joystick movements."""
         if self._joystick != JoystickList.NO_JOYSTICK and self._joystick in (joystick, JoystickList.EVERY_JOYSTICK):
             if tilt:
                 self._tilt_delta = delta
@@ -128,26 +146,30 @@ class PanTiltConstantFilter(VirtualFilter):
 
     @property
     def sixteen_bit_available(self) -> bool:
+        """Check if the filter configured to use sixteen bits."""
         return self._filter_configurations["outputs"] == "both" or self._filter_configurations["outputs"] == "16bit"
 
     @property
     def eight_bit_available(self) -> bool:
+        """Check if the filter configured to use eight bits."""
         return self._filter_configurations["outputs"] == "both" or self._filter_configurations["outputs"] == "8bit"
 
-    def update_time_passed(self) -> None:
+    def _update_time_passed(self) -> None:
         self._pan = min(max(self._pan + 0.01 * self._pan_delta, 0.0), 1.0)
         self._tilt = min(max(self._tilt + 0.01 * self._tilt_delta, 0.0), 1.0)
-        self.notify_observer()
+        self._notify_observer()
 
     def register_observer(self, obs: PanTiltConstantControlUIWidget, callback: Callable[[], None]) -> None:
+        """Register a callback to be notified upon value change."""
         self.observer[obs] = callback
 
-    def notify_observer(self) -> None:
+    def _notify_observer(self) -> None:
         for obs in self.observer:
             (self.observer[obs])()
 
     @property
     def joystick(self) -> JoystickList:
+        """Get or set the joystick associated with this input."""
         return self._joystick
 
     @joystick.setter
@@ -163,4 +185,5 @@ class PanTiltConstantFilter(VirtualFilter):
             self._joystick = joystick
 
     def set_joystick(self, joystick: JoystickList) -> None:
+        """Update a specific joystick."""
         self.joystick = joystick
