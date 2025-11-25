27ef5f4f5a5814b64331109c1015e94523719324
CorsCodini <45790567+CorsCodini@users.noreply.github.com>
2025-11-25 01:16:50 +0100

    chg: correct Datatype

--- src/view/main_widget.py
+++ src/view/main_widget.py
@@ -1,4 +1,4 @@
-"""baseline for all Views """
+"""baseline for all Views"""
 
 from PySide6 import QtWidgets
 from PySide6.QtWidgets import QWidget
@@ -7,9 +7,9 @@ from PySide6.QtWidgets import QWidget
 class MainWidget(QtWidgets.QMainWindow):
     """main View Styling"""
 
-    def __init__(self, widget: QtWidgets, parent: QWidget = None) -> None:
+    def __init__(self, widget: QtWidgets.QWidget, parent: QWidget = None) -> None:
         super().__init__(parent=parent)
-        self._widget: QtWidgets = widget
+        self._widget: QtWidgets.QWidget = widget
         self.setCentralWidget(self._widget)
         self._toolbar = self.addToolBar("widget")
         try:
