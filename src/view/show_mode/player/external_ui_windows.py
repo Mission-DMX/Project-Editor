00880a932a9a6087a14b14e72f5488d4ffa4d6a0
Leon Dietrich <doralitze@chaotikum.org>
2025-11-28 15:49:11 +0100

    add: index page retention

--- src/view/show_mode/player/external_ui_windows.py
+++ src/view/show_mode/player/external_ui_windows.py
@@ -4,42 +4,80 @@ from logging import getLogger
 from typing import TYPE_CHECKING
 
 import PySide6
-from PySide6.QtWidgets import QDialog, QApplication, QVBoxLayout
+from PySide6.QtWidgets import QApplication, QDialog, QVBoxLayout
 
 from controller.network import NetworkManager
 from model import Broadcaster
 from view.show_mode.player.ui_player_widget import UIPlayerWidget
 
 if TYPE_CHECKING:
+    from PySide6.QtWidgets import QWidget
+
     from model import BoardConfiguration
 
 logger = getLogger(__name__)
 
 
 class _ExternalUIWindow(QDialog):
-    def __init__(self, window_index: int, show: BoardConfiguration, parent=None):
+    """Non-Focus window to display additional show UI pages."""
+
+    def __init__(self, window_index: int, show: BoardConfiguration, parent: QWidget | None = None) -> None:
         super().__init__(parent)
         self._show = show
+        self._window_index = window_index
         self.setWindowFlag(PySide6.QtCore.Qt.WindowFlags.WindowCloseButtonHint, False)
-        self.setWindowFlag(PySide6.QtCore.Qt.WindowFlags.Tool | PySide6.QtCore.Qt.WindowStaysOnTopHint | PySide6.QtCore.Qt.WindowDoesNotAcceptFocus)
+        self.setWindowFlag(
+            PySide6.QtCore.Qt.WindowFlags.Tool |
+            PySide6.QtCore.Qt.WindowStaysOnTopHint |
+            PySide6.QtCore.Qt.WindowDoesNotAcceptFocus
+        )
         self.setMinimumSize(800, 600)
         self.setWindowTitle(f"External Window {window_index}")
         screens = QApplication.screens()
         screen = screens[(window_index + 1) % len(screens)]
         self.move(screen.geometry().topLeft())
         self._player_widget = UIPlayerWidget(self)
+        self._player_widget.selected_page_changed.connect(self._player_changed_page)
         layout = QVBoxLayout()
         layout.addWidget(self._player_widget)
         self.setLayout(layout)
         self._switched_to_scene(NetworkManager().current_active_scene_id)
         Broadcaster().active_scene_switched.connect(self._switched_to_scene)
-        # TODO implement ui page per scene retention mechanism
 
-    def _switched_to_scene(self, new_scene_index: int):
+    def _switched_to_scene(self, new_scene_index: int) -> None:
         try:
+            self._player_widget.selected_page_changed_locked = True
+            previous_page_index = self._get_page_index_by_scene(new_scene_index)
             self._player_widget.scene = self._show.scenes[new_scene_index]
+            self._player_widget.goto_page(previous_page_index)
+            QApplication.processEvents()
+            self._player_widget.selected_page_changed_locked = False
+            logger.debug("Scene switch in ui window %s to %s", self._window_index, new_scene_index)
         except IndexError:
-            logger.error(f"Scene {new_scene_index} does not exist in current loaded show file.")
+            logger.error("Scene %s does not exist in current loaded show file.", new_scene_index)
+
+    def _player_changed_page(self, new_index: int) -> None:
+        window_indices = self._parse_indicies_hint()
+        current_scene_index = NetworkManager().current_active_scene_id
+        while len(window_indices) <= current_scene_index:
+            window_indices.append(0)
+        window_indices[current_scene_index] = new_index
+        self._show.ui_hints[f"show_ui_window::{self._window_index}"] = ";".join([str(i) for i in window_indices])
+        logger.debug("Player changed page to page %s. Hints were updated.", new_index)
+
+
+    def _get_page_index_by_scene(self, new_scene_index: int) -> int:
+        window_indices = self._parse_indicies_hint()
+        if new_scene_index >= len(window_indices):
+            return 0
+        return window_indices[new_scene_index]
+
+    def _parse_indicies_hint(self) -> list[int]:
+        hint = self._show.ui_hints.get(f"show_ui_window::{self._window_index}", "")
+        if len(hint) == 0:
+            return []
+        return [int(ss) for ss in hint.split(";")]
+
 
 _current_open_windows: list[_ExternalUIWindow] = []
 
