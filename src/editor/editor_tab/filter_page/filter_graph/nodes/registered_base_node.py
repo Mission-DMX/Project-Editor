bf4e1ff6bc37f1e9b732f1b71c0bbef8f70c4d75
CorsCodini <45790567+CorsCodini@users.noreply.github.com>
2025-12-30 23:19:22 +0100

    feat (RegisteredBaseNode): add filter_representation

--- src/editor/editor_tab/filter_page/filter_graph/nodes/registered_base_node.py
+++ src/editor/editor_tab/filter_page/filter_graph/nodes/registered_base_node.py
@@ -6,15 +6,16 @@ from NodeGraphQt import BaseNode
 
 class RegisteredBaseNode(BaseNode, ABC):
     _Filter_Types: bidict[str, str] = bidict()
-    __representation__ = None
+    __representation__: int = None
 
     def __init_subclass__(cls, **kwargs):
         super().__init_subclass__(**kwargs)
         if cls.__representation__ is not None:
-            cls._Filter_Types.update({cls.type_: cls.__representation__})
+            cls._Filter_Types.update({cls.type_: str(cls.__representation__)})
 
     def __init__(self):
         super().__init__()
+        self._filter_configuration: set[str] = set()
 
     @classmethod
     def get_filter_type(cls, filter_type: str) -> str:
@@ -25,3 +26,7 @@ class RegisteredBaseNode(BaseNode, ABC):
     def get_filter_representation(cls, filter_type: str) -> str:
         """Get filter representation."""
         return cls._Filter_Types[filter_type]
+
+    @property
+    def filter_configuration(self) -> frozenset[str]:
+        return frozenset(self._filter_configuration)
