9d506cfb98c54e5f69fbb6c81c1b562a3bc93766
CorsCodini <45790567+CorsCodini@users.noreply.github.com>
2025-12-31 17:39:06 +0100

    feat (context_menu): add delete nodes

--- src/editor/editor_tab/filter_page/filter_graph/context_menu/context_menu_functions.py
+++ src/editor/editor_tab/filter_page/filter_graph/context_menu/context_menu_functions.py
@@ -1,6 +1,13 @@
 """Functions for the context menu of the nodeditor"""
 
 
+def delete_nodes_and_pipes(graph):
+    """Delete selected nodes and connections."""
+    graph.delete_nodes(graph.selected_nodes())
+    for pipe in graph.selected_pipes():
+        pipe[0].disconnect_from(pipe[1])
+
+
 def toggle_node_search(graph):
     """Show/hide the node search widget."""
     graph.toggle_node_search()
