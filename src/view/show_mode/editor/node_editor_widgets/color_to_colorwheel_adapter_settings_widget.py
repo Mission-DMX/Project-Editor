4a354f3fbbb77bc11054dfe9640a97615dd81fe0
Leon Dietrich <doralitze@chaotikum.org>
2026-03-27 15:11:06 +0100

    add: color mapping list implementation

--- src/view/show_mode/editor/node_editor_widgets/color_to_colorwheel_adapter_settings_widget.py
+++ src/view/show_mode/editor/node_editor_widgets/color_to_colorwheel_adapter_settings_widget.py
@@ -5,14 +5,76 @@ from __future__ import annotations
 from typing import override, TYPE_CHECKING
 
 from PySide6.QtWidgets import QWidget, QFormLayout, QHBoxLayout, QLabel, QPushButton, QComboBox, QCheckBox, QListWidget, \
-    QSpinBox, QSpacerItem, QSizePolicy
+    QSpinBox, QSpacerItem, QSizePolicy, QListWidgetItem, QDialog, QDoubleSpinBox, QDialogButtonBox
 
+from model import ColorHSI
 from model.ofl.fixture import UsedFixture
 from view.show_mode.editor.node_editor_widgets import NodeEditorFilterConfigWidget
+from view.show_mode.show_ui_widgets.debug_viz_widgets import ColorLabel
 
 if TYPE_CHECKING:
     from view.show_mode.editor.nodes import FilterNode
     from model.virtual_filters.color_to_colorwheel import ColorToColorWheel, extract_colorwheel_mappings_from_fixture
+
+
+class _ColorMappingListWidgetItem(QListWidgetItem):
+    """Purpose of this widget is to display a single mapping."""
+
+    def __init__(self, parent: QListWidget, color: ColorHSI, slot_value: int):
+        """Initializes and adds the widget based on the provided color and slot value."""
+        super().__init__()
+        self.color = color
+        self.slot_value = slot_value
+
+        widget = QWidget()
+        widget.setLayout(QHBoxLayout())
+        self._color_label = ColorLabel()
+        self._color_label.set_color(self.color)
+        widget.layout().addWidget(self._color_label)
+        self._slot_label = QLabel(str(slot_value))
+        widget.layout().addWidget(self._slot_label)
+
+        parent.addItem(self)
+        parent.setItemWidget(self, widget)
+        self.setSizeHint(widget.sizeHint())
+
+
+class _ColorSlotInputDialog(QDialog):
+    """Query a color and a slot."""
+
+    def __init__(self, parent: QWidget, list_widget: QListWidget):
+        """Initializes the dialog."""
+        super().__init__(parent)
+
+        self._list_widget = list_widget
+
+        layout = QFormLayout()
+        self._hue_tb = QDoubleSpinBox()
+        self._hue_tb.setRange(0, 360)
+        self._hue_tb.setDecimals(2)
+        layout.addRow("Hue", self._hue_tb)
+        self._saturation_tb = QDoubleSpinBox()
+        self._saturation_tb.setDecimals(5)
+        self._saturation_tb.setValue(1.0)
+        self._saturation_tb.setRange(0, 1)
+        layout.addRow("Saturation", self._saturation_tb)
+        self._slot_tb = QSpinBox()
+        self._slot_tb.setRange(0, 65565)
+        layout.addRow("Slot", self._slot_tb)
+        layout.addItem(QSpacerItem(0, 25))
+
+        self._button_box = QDialogButtonBox()
+        self._button_box.addButton(QDialogButtonBox.StandardButton.Cancel).clicked.connect(self.close)
+        self._button_box.addButton(QDialogButtonBox.StandardButton.Ok).clicked.connect(self.accept)
+        layout.addWidget(self._button_box)
+        self.setLayout(layout)
+
+    @override
+    def accept(self):
+        _ColorMappingListWidgetItem(self._list_widget,
+                                    ColorHSI(self._hue_tb.value(), self._saturation_tb.value(), 0.5),
+                                    self._slot_tb.value())
+        super().accept()
 
 
 class ColorToColorwheelAdapterSetupWidget(NodeEditorFilterConfigWidget):
@@ -21,6 +83,7 @@ class ColorToColorwheelAdapterSetupWidget(NodeEditorFilterConfigWidget):
     def __init__(self, filter: ColorToColorWheel) -> None:
         """Initialize the configuration widget."""
         super().__init__()
+        self._input_dialog: _ColorSlotInputDialog | None = None
         self._widget = QWidget()
         layout = QFormLayout()
 
@@ -57,7 +120,18 @@ class ColorToColorwheelAdapterSetupWidget(NodeEditorFilterConfigWidget):
 
         self._color_mapping_list = QListWidget()
         layout.addWidget(self._color_mapping_list)
-        # TODO add buttons for addition and removal of mappings
+        self._mapping_manipulation_buttongroup = QWidget()
+        self._mapping_manipulation_buttongroup.setLayout(QHBoxLayout())
+        self._mapping_manipulation_buttongroup.layout().addItem(QSpacerItem(
+            10, 0, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed
+        ))
+        self._add_mapping_button = QPushButton("Add Mapping")
+        self._add_mapping_button.clicked.connect(self._add_mapping_clicked)
+        self._mapping_manipulation_buttongroup.layout().addWidget(self._add_mapping_button)
+        self._remove_mapping_button = QPushButton("Remove Mapping")
+        self._remove_mapping_button.clicked.connect(self._remove_mapping_clicked)
+        self._mapping_manipulation_buttongroup.layout().addWidget(self._remove_mapping_button)
+        layout.addWidget(self._mapping_manipulation_buttongroup)
 
         self._wheel_speed_spinbox = QSpinBox()
         self._wheel_speed_spinbox.setMinimum(0)
@@ -71,13 +145,29 @@ class ColorToColorwheelAdapterSetupWidget(NodeEditorFilterConfigWidget):
 
         self._widget.setLayout(layout)
 
+    def _add_mapping_clicked(self) -> None:
+        self._input_dialog = _ColorSlotInputDialog(self._widget, self._color_mapping_list)
+        self._input_dialog.setModal(True)
+        self._input_dialog.show()
+
+    def _remove_mapping_clicked(self) -> None:
+        items_to_remove = [index for index in self._color_mapping_list.selectedIndexes()]
+        items_to_remove.sort(reverse=True)
+        for item in items_to_remove:
+            self._color_mapping_list.takeItem(item)
+
     def _load_from_fixture_clicked(self) -> None:
         # TODO show selection dialog with callback self._update_selected_fixture
         pass
 
     def _parse_color_mapping(self, mapping: str) -> None:
-        # TODO clear and populate color mapping table
-        pass
+        self._color_mapping_list.clear()
+        for entry_str in mapping.split(';'):
+            hue, saturation, slot = entry_str.split(':')
+            hue = float(hue)
+            saturation = float(saturation)
+            slot = int(slot)
+            _ColorMappingListWidgetItem(self._color_mapping_list, ColorHSI(hue, saturation, 0.5), slot)
 
     def _update_selected_fixture(self) -> None:
         if self._selected_fixture is None:
@@ -85,19 +175,28 @@ class ColorToColorwheelAdapterSetupWidget(NodeEditorFilterConfigWidget):
             self._colorwheel_index_spinbox.setEnabled(False)
             self._color_mapping_list.setEnabled(True)
             self._selected_fixture_label.setText("No Fixture Selected.")
+            self._remove_mapping_button.setEnabled(True)
+            self._add_mapping_button.setEnabled(True)
         else:
             self._fixture_selection_or_clear_button.setText("Clear Selected Fixture")
             self._colorwheel_index_spinbox.setEnabled(len(self._selected_fixture.colorwheel_mappings) > 0)
             self._color_mapping_list.setEnabled(False)
             self._selected_fixture_label.setText(self._selected_fixture.name)
-        # TODO disable mapping management buttons if selected fixture is not None
+            self._remove_mapping_button.setEnabled(False)
+            self._add_mapping_button.setEnabled(False)
         self._parse_color_mapping(extract_colorwheel_mappings_from_fixture(
             self._selected_fixture,
             selected_slot_index=self._colorwheel_index_spinbox.value()
         ))
 
     def _compile_color_mapping_string(self) -> str:
-        return ""  # TODO
+        parts: list[_ColorMappingListWidgetItem] = []
+        for i in range(self._color_mapping_list.count()):
+            item = self._color_mapping_list.item(i)
+            if not isinstance(item, _ColorMappingListWidgetItem):
+                continue
+            parts.append(item)
+        return ";".join(f"{item.color.hue}:{item.color.saturation}:{item.slot_value}" for item in parts)
 
     @override
     def get_widget(self) -> QWidget:
