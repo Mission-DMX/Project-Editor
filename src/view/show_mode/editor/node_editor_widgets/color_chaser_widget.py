84c96cc09982a899e3304c6a48b0f3589acad8a1
Leon Dietrich <doralitze@chaotikum.org>
2026-05-09 08:44:41 +0200

    add: event selection button

--- src/view/show_mode/editor/node_editor_widgets/color_chaser_widget.py
+++ src/view/show_mode/editor/node_editor_widgets/color_chaser_widget.py
@@ -15,10 +15,12 @@ from PySide6.QtGui import QMovie, QImage, Qt
 from PySide6.QtWidgets import QWidget, QVBoxLayout, QSlider, QFormLayout, QSplitter, QLabel, QListWidget, QHBoxLayout, \
     QPushButton, QSpinBox
 
+from model.events import EventFilter
 from model.filter_data.chaser_model import ChaserModel, ChaserConfig
 from utility import resource_path
 from view.show_mode.editor.node_editor_widgets import NodeEditorFilterConfigWidget
 from view.show_mode.editor.node_editor_widgets.cue_editor.yes_no_dialog import YesNoDialog
+from view.show_mode.editor.node_editor_widgets.sequencer_editor.event_selection_dialog import EventSelectionDialog
 
 if TYPE_CHECKING:
     from PySide6.QtWidgets import QDialog
@@ -183,7 +185,16 @@ class ColorChaserFilterConfigWidget(NodeEditorFilterConfigWidget):
         general_settings_layout = QFormLayout()
         self._number_of_pixels_tb = QSpinBox()
         general_settings_layout.addRow("Number of Pixels: ", self._number_of_pixels_tb)
-        # TODO add trigger event selector
+        event_selection_container = QWidget()
+        event_selection_layout = QHBoxLayout()
+        self._event_label = QLabel("Time Mode")
+        event_selection_layout.addWidget(self._event_label)
+        event_selection_layout.addStretch()
+        self._event_select_clear_button = QPushButton("Select Event")
+        self._event_select_clear_button.clicked = self._event_select_or_clear_clicked
+        event_selection_layout.addWidget(self._event_select_clear_button)
+        event_selection_container.setLayout(event_selection_layout)
+        general_settings_layout.addRow("Trigger Event: ", event_selection_container)
         # TODO add widgets to manage color parameters
         # TODO add widgets to manage number parameters
         top_layout.addLayout(general_settings_layout)
@@ -258,3 +269,25 @@ class ColorChaserFilterConfigWidget(NodeEditorFilterConfigWidget):
         self._input_dialog = YesNoDialog(
             self.get_widget(), "Preview", "Would you like to switch to live preview?", self._enable_live_updates
         )
+
+    def _event_select_or_clear_clicked(self):
+        if self._model.trigger_event is None:
+            self._input_dialog = EventSelectionDialog(self._widget)
+            self._input_dialog.accepted.connect(self._event_selected_callback)
+            self._input_dialog.show()
+        else:
+            self._event_label.setText("Time Mode")
+            self._event_select_clear_button.setText("Select Event")
+            self._model.trigger_event = None
+        pass
+
+    def _event_selected_callback(self):
+        if not isinstance(self._input_dialog, EventSelectionDialog):
+            logger.critical("Event selection dialog expected.")
+            return
+        sender, function, args = self._input_dialog.selected_event
+        self._event_label.setText(f"Trigger Event: {sender}:{function} -> {args}")
+        self._event_select_clear_button.setText("Clear")
+        self._model.trigger_event = EventFilter(sender, function, [ord(c) for c in args])
+        self._input_dialog.deleteLater()
+        self._input_dialog = None
