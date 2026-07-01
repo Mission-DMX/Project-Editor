e9e4ecc8f71d607c6e7193f0b5913325026bcc2a
Leon Dietrich <doralitze@chaotikum.org>
2026-07-01 15:35:43 +0200

    fix: resource path lookup for compiled version

--- src/utility.py
+++ src/utility.py
@@ -5,5 +5,9 @@ import sys
 
 def resource_path(relative_path: str) -> str:
     """ Gibt den absoluten Pfad zur Ressource zurück. Funktioniert im Skript und im PyInstaller-Executable """
-    base_path = getattr(sys, "_MEIPASS", os.path.abspath("."))
-    return os.path.join(base_path, relative_path)
+    if "__compiled__" in globals():
+        file_path = os.path.join(os.path.dirname(sys.argv[0]), relative_path)
+    else:
+        base_path = getattr(sys, "_MEIPASS", os.path.abspath("."))
+        file_path = os.path.join(base_path, relative_path)
+    return file_path
