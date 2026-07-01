a9afb3aedc1a847eb32559d5f2de293441a38860
Leon Dietrich <doralitze@chaotikum.org>
2026-07-01 15:49:02 +0200

    fix: docstrings

--- src/utility.py
+++ src/utility.py
@@ -1,10 +1,10 @@
-"""utility functions"""
+"""Startup utility functions."""
 import os
 import sys
 
 
 def resource_path(relative_path: str) -> str:
-    """ Gibt den absoluten Pfad zur Ressource zurück. Funktioniert im Skript und im PyInstaller-Executable """
+    """Resolve resource paths for the given installation method."""
     if "__compiled__" in globals():
         file_path = os.path.join(os.path.dirname(sys.argv[0]), relative_path)
     else:
