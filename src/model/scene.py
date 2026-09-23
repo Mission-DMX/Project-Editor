e2794a1e90703026121e0d9ba6e8b036f5e17c81
CorsCodini <45790567+CorsCodini@users.noreply.github.com>
2026-09-23 12:29:00 +0200

    style: reformat inputs

--- src/model/scene.py
+++ src/model/scene.py
@@ -6,9 +6,9 @@ from typing import TYPE_CHECKING, NamedTuple, override
 
 from PySide6.QtCore import QObject, Signal
 
-from .universe import Universe
-
 from controller.utils.graph_sorting import layered_layout
+
+from .universe import Universe
 
 if TYPE_CHECKING:
     from .board_configuration import BoardConfiguration
@@ -143,8 +143,9 @@ class Scene(QObject):
         """Get the list of default values to be applied on scene switch."""
         return self._dmx_default_values.copy()
 
-    def insert_dmx_default_value(self, universe: Universe | int, channel: int, value: int,
-                                 supress_emission: bool = False) -> bool:
+    def insert_dmx_default_value(
+        self, universe: Universe | int, channel: int, value: int, supress_emission: bool = False
+    ) -> bool:
         """Add a new default value to the scene.
 
         Existing values will be updated.
@@ -183,8 +184,9 @@ class Scene(QObject):
 
         """
         universe_id = universe.id if isinstance(universe, Universe) else universe
-        values_to_remove = [val for val in self._dmx_default_values if
-                            val.universe_id == universe_id and val.channel == channel]
+        values_to_remove = [
+            val for val in self._dmx_default_values if val.universe_id == universe_id and val.channel == channel
+        ]
         for item in values_to_remove:
             self._dmx_default_values.remove(item)
         if len(values_to_remove) > 0 and not supress_emission:
