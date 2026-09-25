d037940406c852287c247b0a3cfe34ff25a4b022
Doralitze <doralitze@chaotikum.org>
2026-09-25 09:28:56 +0200

    chg: graph sorting to be in model

--- src/model/scene.py
+++ src/model/scene.py
@@ -6,7 +6,7 @@ from typing import TYPE_CHECKING, NamedTuple, override
 
 from PySide6.QtCore import QObject, Signal
 
-from controller.utils.graph_sorting import layered_layout
+from model.utils.graph_sorting import layered_layout
 
 from .universe import Universe
 
